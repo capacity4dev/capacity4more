d9cb16fadafd05691beabd8bec749a6860a57e27
Itamar Shapira Bar-Lev <itamar@gizra.com>
2014-07-01 15:24:04 +0300

    Updating message template

--- capacity4more/modules/c4m/c4m_message/message.tpl.php
+++ capacity4more/modules/c4m/c4m_message/message.tpl.php
@@ -1,9 +1,20 @@
-<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <span class="profile-pic">
+<div class="row post <?php print $message_type; ?> <?php print $node_type; ?>">
+  <div class="col-md-3 user-info">
     <?php echo $user_picture; ?>
-  </span>
-
-  <h6 class="operation <?php print $activity_class; ?>"><?php print render($content); ?></h6>
-
-  <div class="details"><span class="performer"><?php print $user_name; ?></span>,<span class="date"><?php print $created; ?></span></div>
+  </div>
+  <div class="col-md-9 content-wrapper">
+    <div class="post-action">
+      <span class="icon"></span>
+      <span class="author sans-font"><?php print $user_name; ?></span>
+      <span class="action"><?php print render($content); ?></span>
+      <span class="date"><?php print $created; ?></span>
+    </div>
+    <div class="post-content">
+      <?php print $media; ?>
+      <?php print $body; ?>
+      <?php if ($comments_count): ?>
+        <p class="comments"><?php print $comments_count; ?></p>
+      <?php endif; ?>
+    </div>
+  </div>
 </div>
