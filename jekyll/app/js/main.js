b3d2b925e96eb0ec0e017320e4239668140be7f1
Nader Safadi <nader@gizra.com>
2014-08-05 12:20:28 +0300

    Responsive fixes for homepage

--- jekyll/app/js/main.js
+++ jekyll/app/js/main.js
@@ -1,4 +1,5 @@
 jQuery( document ).ready(function() {
+
   // Listen to watch video button click.
   jQuery( "#watch-video-link" ).click(function() {
     jQuery( "#homepage-banner").hide('fast');
@@ -6,8 +7,8 @@ jQuery( document ).ready(function() {
     jQuery( "#homepage-video").show('fast');
     document.getElementById("homepage-video").play();
   });
-  // Listen to close div click.
 
+  // Listen to close div click.
   jQuery( "#close" ).click(function() {
     jQuery( "#about").hide('fast');
   });
