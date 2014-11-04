a0af7d6b095cab3bc6405f181da94ca17a19410c
Helena Eksler <helena@gizra.com>
2014-11-04 17:20:59 +0200

    Message theming.

---
+++ capacity4more/modules/c4m/message/c4m_message/templates/node--document--activity_stream.tpl.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Display a node in "Activity stream" view mode.
+ *
+ * @see node.tpl.php
+ */
+?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+      print $description;
+    ?>
+  </div>
+  <div class="row">
+    <?php print $download_link; ?>
+  </div>
+  <div class="row">
+    <?php print t('Filetype: @filetype | Filesize: @filesize', array('@filetype' => $document['type'], '@filesize' => $file_size)); ?>
+  </div>
+
+</div>
