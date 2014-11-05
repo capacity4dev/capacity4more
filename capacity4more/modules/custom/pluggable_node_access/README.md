cf314a59f36e118acceb4b6f7c56b24a301658b8
Shushu Inbar <shushu@gizra.com>
2014-11-05 09:50:24 +0200

    WIP.

---
+++ capacity4more/modules/custom/pluggable_node_access/README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/Gizra/pluggable_node_access.svg?branch=7.x-1.x)](https://travis-ci.org/Gizra/pluggable_node_access)
+
+Add custom logic to viewing OG groups.
---
+++ capacity4more/modules/custom/pluggable_node_access/includes/PluggableNodeAccess.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains \PluggableNodeAccess.
+ */
+
+
+class PluggableNodeAccess extends Entity {
+
+  public function __construct($values = array()) {
+    parent::__construct($values, 'pluggable_node_access');
+    if (!isset($this->timestamp)) {
+      $this->timestamp = time();
+    }
+  }
+}
