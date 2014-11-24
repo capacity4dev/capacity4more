f974b1a3fa9606bfebe43c8d445852ed01982653
Helena Eksler <helena@gizra.com>
2014-11-24 10:26:18 +0200

    Possibility to load multiple documents.

--- capacity4more/modules/c4m/restful/c4m_restful_quick_post/templates/quick-post-form.tpl.php
+++ capacity4more/modules/c4m/restful/c4m_restful_quick_post/templates/quick-post-form.tpl.php
@@ -110,7 +110,7 @@
         <related-documents documents="data.related_document"></related-documents>
         <div ng-show="dropSupported" class="form-control drop-box" ng-file-drop="onFileSelect($files);" ng-file-drop-available="dropSupported=true" ng-file-drag-over-class="file-upload-drag">
 
-          <div ng-hide="serverSide.file">
+          <div>
             <?php print t('Drop file here to upload or'); ?>
             <a href="javascript://" ng-click="browseFiles()"><?php print t('Browse') ?></a>
             <input type="file" ng-hide="true" name="document-file" id="document_file" ng-file-select="onFileSelect($files)">
