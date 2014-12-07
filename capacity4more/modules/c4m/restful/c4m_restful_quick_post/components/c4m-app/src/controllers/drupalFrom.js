9bbfba67aebb13d1b97ac170034b3c21432e21fb
Helena Eksler <helena@gizra.com>
2014-12-07 18:18:51 +0200

    WIP

--- capacity4more/modules/c4m/restful/c4m_restful_quick_post/components/c4m-app/src/controllers/drupalFrom.js
+++ capacity4more/modules/c4m/restful/c4m_restful_quick_post/components/c4m-app/src/controllers/drupalFrom.js
@@ -1,17 +1,18 @@
 'use strict';
 
 angular.module('c4mApp')
-  .controller('DrupalFromCtrl', function($scope, DrupalSettings, $document, QuickPostService) {
+  .controller('DrupalFromCtrl', function($scope, DrupalSettings, EntityResource, Request, $window, $document, $modal, QuickPostService, $interval, $sce, FileUpload) {
 
     $scope.data = DrupalSettings.getData('vocabularies');
 
+    $scope.popups = [];
     angular.forEach($scope.data, function(value, key) {
-      $scope.popups[value] = 0;
+      $scope.popups.language = 0;
     });
 
     // Toggle the visibility of the popovers.
-    $scope.togglePopover = function(name, event) {
-      QuickPostService.togglePopover(name, event, $scope.popups);
+    $scope.togglePopover = function() {
+      $scope.popups.language = $scope.popups.language == 1 ? 0 : 1;
     };
 
     // Close all popovers on "ESC" key press.
@@ -19,6 +20,9 @@ angular.module('c4mApp')
       QuickPostService.keyUpHandler(keyEvent, $scope);
     };
 
+    $scope.updateSelectedTerms = function(key) {
+        jQuery('input[type=checkbox][value="' + key + '"]').attr("checked", "true");
+    };
     // Call the keyUpHandler function on key-up.
     $document.on('keyup', $scope.keyUpHandler);
   });
