8fa330b88bb89ade861cf61b28c4c97b0ecafa11
Kevin Van Ransbeeck <Kevin.VanRansbeeck@amplexor.com>
2014-12-09 17:18:08 +0100

    Bundle added

--- capacity4more/themes/c4m/kapablo/config.rb
+++ capacity4more/themes/c4m/kapablo/config.rb
@@ -4,6 +4,9 @@ sass_dir        = "sass"
 images_dir      = "images"
 javascripts_dir = "js"
 
+# Require any additional compass plugins installed on your system.
+require 'breakpoint'
+
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
 # the absolute path to the theme from the server root.
