6539a0392919b4b106a6d29b8cd804641ab64a44
Kevin Van Ransbeeck <Kevin.VanRansbeeck@amplexor.com>
2014-12-09 16:31:35 +0100

    Basic structure + Grunt setup for svg icons with fallback (submenu header)

--- capacity4more/themes/c4m/kapablo/Gruntfile.js
+++ capacity4more/themes/c4m/kapablo/Gruntfile.js
@@ -1,33 +1,136 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            dist: {
-                files: {
-                    'css/style.css': 'sass/style.scss'
-                },
+
+        // SCSS
+        compass: {
+            build: {
                 options: {
-                    style: 'compressed', // Can be nested, compact, compressed, expanded.
-                    sourcemap: 'none'
+                    outputStyle: 'expanded',
+                    debugInfo: true,
+                    environment: 'development'
                 }
             }
         },
+
+//        sass: {
+//            dist: {
+//                files: {
+//                    'css/style.css': 'sass/style.scss'
+//                },
+//                options: {
+//                    style: 'compressed', // Can be nested, compact, compressed, expanded.
+//                    sourcemap: 'none'
+//                }
+//            }
+//        },
+
+        // SVG Minification
+        svgmin: {
+            multiple: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'images/svg/',
+                        src: ['**/*.svg'],
+                        dest: 'images/svgmin'
+                    }
+                ]
+            }
+        },
+
+        // SVG Fallback
+        grunticon: {
+            icons: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'images/svgmin/icons',
+                        src: ['**/*.svg'],
+                        dest: 'images/icons'
+                    }
+                ]
+            }
+        },
+
+        // JS
+        concat: {
+            options: {
+                stripBanners: true
+            },
+            app: {
+                src: [
+                    'js/app/kapablo.js',
+                    'js/app/modernizr.js'
+                ],
+                dest: 'js/<%= pkg.name %>.concat.js'
+            }
+        },
+        uglify: {
+            options: {
+                report: 'min'
+            },
+            app: {
+                src: ['<%= concat.app.dest %>'],
+                dest: 'js/<%= pkg.name %>.min.js'
+            }
+//            ie7: {
+//                src: ['js/app/ie7.js'],
+//                dest: 'js/<%= pkg.name %>.ie7.min.js'
+//            }
+        },
+
         watch: {
-            css: {
-                files: '**/*.scss',
-                tasks: ['sass']
+//            css: {
+//                files: '**/*.scss',
+//                tasks: ['sass']
+//            },
+            compass: {
+                files: ['sass/*.scss', 'sass/**/*.scss'],
+                tasks: ['compass'],
+                options: {
+                    livereload: true
+                }
+            },
+
+            svgmin: {
+                files: ['images/svg/**/*.svg'],
+                tasks: ['svgmin', 'grunticon'],
+                options: {
+                    livereload: true
+                }
+            },
+
+            scripts: {
+                files: ['js/app/*.js'],
+                tasks: ['concat:app', 'uglify:app'],
+                options: {
+                    livereload: true
+                }
             }
         }
     });
-    grunt.loadNpmTasks('grunt-contrib-sass');
+
+//    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-svgmin');
+    grunt.loadNpmTasks('grunt-grunticon');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
 
     grunt.registerTask('build', [
-        'sass'
+        'concat',
+        'uglify',
+        'svgmin',
+        'grunticon',
+        'compass'
+//        'sass'
     ]);
 
     grunt.registerTask('default', [
-        'sass',
+        'build',
         'watch'
     ]);
 }
---
+++ capacity4more/themes/c4m/kapablo/config.rb
@@ -0,0 +1,13 @@
+# Set this to the root of your project when deployed:
+css_dir         = "css"
+sass_dir        = "sass"
+images_dir      = "images"
+javascripts_dir = "js"
+
+# Require any additional compass plugins installed on your system.
+require 'breakpoint'
+
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from the server root.
+relative_assets = true
