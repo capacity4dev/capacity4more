c94ac1b0e59137472d0c357f0d8a325f2604a72e
Kevin Van Ransbeeck <Kevin.VanRansbeeck@amplexor.com>
2014-12-10 13:14:27 +0100

    Make the og menu basic responsive

--- capacity4more/themes/c4m/kapablo/templates/page.tpl.php
+++ capacity4more/themes/c4m/kapablo/templates/page.tpl.php
@@ -93,45 +93,43 @@
   <div id="navbar">
     <div class="container">
       <div class="row">
-        <div class="col-sm-12">
-          <div class="navbar-header">
-            <?php if ($logo): ?>
-              <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-                <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
-              </a>
-            <?php endif; ?>
-
-            <?php if (!empty($site_name)): ?>
-              <a class="name navbar-brand" href="<?php print $front_page; ?>"
-                 title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-            <?php endif; ?>
-
-            <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-          </div>
-
-          <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-            <div class="navbar-collapse collapse">
-              <nav role="navigation">
-                <?php if (!empty($primary_nav)): ?>
-                  <?php print render($primary_nav); ?>
-                <?php endif; ?>
-                <?php if (!empty($secondary_nav)): ?>
-                  <?php print render($secondary_nav); ?>
-                <?php endif; ?>
-                <?php if (!empty($page['navigation'])): ?>
-                  <?php print render($page['navigation']); ?>
-                <?php endif; ?>
-              </nav>
-            </div>
+        <div class="col-sm-12 col-md-6 header-logo">
+          <?php if ($logo): ?>
+            <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
+            </a>
           <?php endif; ?>
         </div>
+        <div class="col-sm-12 col-md-6 header-search">
+<!--          SEARCH-->
+        </div>
       </div>
+
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+
+        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+
+        <div class="navbar-collapse collapse">
+          <nav role="navigation">
+            <?php if (!empty($primary_nav)): ?>
+              <?php print render($primary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($secondary_nav)): ?>
+              <?php print render($secondary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($page['navigation'])): ?>
+              <?php print render($page['navigation']); ?>
+            <?php endif; ?>
+          </nav>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
 </header>
@@ -148,11 +146,7 @@
   <!-- /#page-header -->
 
   <?php if (!empty($page['content_top'])): ?>
-    <div class="row">
-      <div class="col-sm-12">
-        <?php print render($page['content_top']); ?>
-      </div>
-    </div>
+    <?php print render($page['content_top']); ?>
   <?php endif; ?>
 
   <div class="row">
