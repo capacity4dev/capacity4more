dee14a7bc4d230a54cf6d655d44b969a1241eb84
Kevin Van Ransbeeck <Kevin.VanRansbeeck@amplexor.com>
2014-12-10 15:19:41 +0100

    Padding in panels blocks on the activity stream (group dashboard)

--- capacity4more/themes/c4m/kapablo/templates/message--activity_stream.tpl.php
+++ capacity4more/themes/c4m/kapablo/templates/message--activity_stream.tpl.php
@@ -1,9 +1,9 @@
 <div class="row">
-  <div class="col-sm-4">
+  <div class="col-sm-4 col-md-2">
     <?php print $icon; ?>
   </div>
 
-  <div class="col-sm-8">
+  <div class="col-sm-8 col-md-10">
     <?php print $content; ?>
   </div>
 </div>
