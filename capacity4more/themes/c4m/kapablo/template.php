84bf6e41f51ce508f2c066f946fc7a70577d53e1
Joachim Willems <joachim.willems@amplexor.com>
2014-12-11 17:10:13 +0100

    Overriding bootstrap book overrides.

--- capacity4more/themes/c4m/kapablo/template.php
+++ capacity4more/themes/c4m/kapablo/template.php
@@ -59,3 +59,45 @@ function kapablo_preprocess_message(&$variables) {
   );
   $variables['icon'] = theme('image', $icon_variables);
 }
+
+/**
+ * Overrides theme_menu_tree() for book module.
+ */
+function kapablo_menu_tree__book_toc(&$variables) {
+  $output = '<div class="book-toc">';
+  $output .= '<ul role="menu">' . $variables['tree'] . '</ul>';
+  $output .= '</div>';
+  return $output;
+}
+
+/**
+ * Overrides theme_menu_tree() for book module.
+ */
+function kapablo_menu_tree__book_toc__sub_menu(&$variables) {
+  return '<ul class="menu" role="menu">' . $variables['tree'] . '</ul>';
+}
+
+/**
+ * Overrides theme_menu_link() for book module.
+ */
+function kapablo_menu_link__book_toc(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = drupal_render($element['#below']);
+  $element['#attributes']['role'] = 'presentation';
+  $link = TRUE;
+  if ($element['#title'] && $element['#href'] === FALSE) {
+    $element['#attributes']['class'][] = 'dropdown-header';
+    $link = FALSE;
+  }
+  elseif ($element['#title'] === FALSE && $element['#href'] === FALSE) {
+    $element['#attributes']['class'][] = 'divider';
+    $link = FALSE;
+  }
+  elseif (($element['#href'] == $_GET['q'] || ($element['#href'] == '<front>' && drupal_is_front_page())) && (empty($element['#localized_options']['language']))) {
+    $element['#attributes']['class'][] = 'active';
+  }
+  if ($link) {
+    $element['#title'] = l($element['#title'], $element['#href'], $element['#localized_options']);
+  }
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $element['#title'] . $sub_menu . "</li>\n";
+}
