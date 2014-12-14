5e8e908c07f7105ee4ac5167f9b3b003bc52cb70
Helena Eksler <helena@gizra.com>
2014-12-14 15:33:05 +0200

    Show values.

--- capacity4more/modules/c4m/vocab/c4m_vocab_language/templates/c4m_vocab_widget.tpl.php
+++ capacity4more/modules/c4m/vocab/c4m_vocab_language/templates/c4m_vocab_widget.tpl.php
@@ -3,6 +3,21 @@
     <div>
       <button type="button" ng-click="togglePopover('<?php print($vocabulary_name); ?>', $event)" class="btn btn-primary fa fa-plus">&nbsp;<?php print(t('Select @name', array('@name' => str_replace("c4m_vocab_", "", $vocabulary_name)))); ?></button>
     </div>
+    <div class="selected-values" ng-show="data.<?php print($vocabulary_name); ?>">
+      <div class="value" ng-show="value === true && data.<?php print($vocabulary_name); ?>[key]" ng-repeat="(key, value) in model">
+        <div class="parent col-sm-6">
+          <span>
+            <i ng-click="removeTaxonomyValue(key, '<?php print($vocabulary_name); ?>')" class="fa fa-times"></i> {{ findLabel(data.<?php print($vocabulary_name); ?>, key) }}
+            <i ng-show="data.<?php print($vocabulary_name); ?>[key]" class="fa fa-chevron-right "></i>
+          </span>
+        </div>
+        <div class="child col-sm-6">
+          <span ng-show="model[child.id] === true" ng-repeat="(childkey, child) in data.<?php print($vocabulary_name); ?>[key].children">
+            <i ng-click="removeTaxonomyValue(child.id, '<?php print($vocabulary_name); ?>')" class="fa fa-times"></i> {{ findLabel(data.<?php print($vocabulary_name); ?>, child.id) }}
+          </span>
+        </div>
+      </div>
+    </div>
     <!-- Hidden language checkboxes.-->
     <div class="popover right hidden-checkboxes" ng-hide="popups.<?php print($vocabulary_name); ?> != 1">
       <div class="arrow"></div>
