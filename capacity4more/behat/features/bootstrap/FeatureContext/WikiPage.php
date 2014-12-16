8043a69ee5c647b962efef69b2ce07bab9c193e6
Joachim Willems <joachim.willems@amplexor.com>
2014-12-16 14:38:46 +0100

    DS settings for wiki pages. Query alter to allow unpublished wiki pages in book outline. Add weight of elements back in a hook_ds_pre_render because they sometimes get lost.

--- capacity4more/behat/features/bootstrap/FeatureContext/WikiPage.php
+++ capacity4more/behat/features/bootstrap/FeatureContext/WikiPage.php
@@ -25,8 +25,38 @@ trait WikiPage {
     $steps[] = new Step\When('I should see a "Title in right region" field');
     $steps[] = new Step\When('I should see a "Author" field');
     $steps[] = new Step\When('I should see a "Comment" field');
-
+    $steps[] = new Step\When('I should see a Wiki "table of contents" in the "left" bar');
+    $steps[] = new Step\When('I should see a Wiki "navigation" in the "right" bar');
     return $steps;
+  }
+
+  /**
+   * @Given /^I should see a Wiki "([^"]*)" in the "([^"]*)" bar$/
+   */
+  public function iShouldSeeAWikiInTheBar($wrapper, $region) {
+    $element = NULL;
+    $page = $this->getSession()->getPage();
+
+    $wrapper_class = '.book-';
+
+    switch($wrapper) {
+      case 'table of contents':
+        $wrapper_class .= 'toc';
+        break;
+      case 'navigation':
+        $wrapper_class .= 'navigation';
+        break;
+    }
+
+    $locator = '.region-content .group-' . $region . ' ' . $wrapper_class ;
+
+    if (!empty($locator)) {
+      $element = $page->findAll('css', $locator);
+    }
+
+    if (!count($element)) {
+      throw new \Exception("No Wiki $wrapper found in $region region.");
+    }
   }
 
 }
--- capacity4more/drupal-org-core.make
+++ capacity4more/drupal-org-core.make
@@ -7,3 +7,4 @@ projects[drupal][version] = "7.34"
 ; Patch to make it possible to run tests on modules located in a custom profile.
 ; See https://drupal.org/node/911354#comment-7341092.
 projects[drupal][patch][] = "https://drupal.org/files/911354-drupal-profile-85.patch"
+projects[drupal][patch][] = "https://www.drupal.org/files/issues/book_unpublished_patch_760102-10.patch"
