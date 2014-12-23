3bc5331efa0e09089a55350a40b602d3108bd09b
Helena Eksler <helena@gizra.com>
2014-12-23 15:09:34 +0200

    Update js.

--- capacity4more/modules/c4m/content/c4m_content_discussion/js/document_overlay.js
+++ capacity4more/modules/c4m/content/c4m_content_discussion/js/document_overlay.js
@@ -1,9 +1,11 @@
 (function ($) {
 
-  console.log('outside');
+  $(document).bind('drupalOverlayLoad', function() {
+    // Populate the entity reference field with the selected node ID.
+    $('.page-title', parent.window.document).hide();
+  });
 
   Drupal.overlay.eventhandlerDocumentChosen = function (event) {
-    alert('hello');
     if (event.type == 'click') {
       $('#edit-c4m-related-document-und-0-target-id').val('we are here');
       $.bbq.removeState('overlay');
