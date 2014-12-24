8a39a240ae9241ff0f0ed10c36edf861a3d337ad
Helena Eksler <helena@gizra.com>
2014-12-24 17:03:16 +0200

    WIP

--- capacity4more/modules/c4m/content/c4m_content_discussion/templates/c4m_content_discussion_add_document.tpl.php
+++ capacity4more/modules/c4m/content/c4m_content_discussion/templates/c4m_content_discussion_add_document.tpl.php
@@ -1,1 +1,15 @@
-<?php print $fileId . ' is number of the file';
+<form name="documentForm" ng-submit="CreateDocument()">
+
+  <?php
+    print('Upload new document: <br/>');
+    print($file);
+  ?>
+
+  <br/>
+  <input id="label" class="form-control" name="label" type="text" placeholder="<?php print t('Add document title'); ?>" ng-minlength=3>
+
+  <p>Add this document also to the group Library?</p>
+  <button type="submit" id="save-edit" class="btn btn-primary" tabindex="100"><?php print t('YES'); ?></button>
+  <button type="submit" id="save" class="btn btn-primary" tabindex="100"><?php print t('NO'); ?></button>
+  <a href="javascript://" id="clear-button" ng-click="cancel()"><?php print t('Cancel'); ?></a>
+</form>
