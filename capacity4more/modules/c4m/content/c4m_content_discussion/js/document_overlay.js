48a0bdc2ad63031ca8e458f8d22dd733cfc8d366
Helena Eksler <helena@gizra.com>
2014-12-24 11:41:24 +0200

    WIP

--- capacity4more/modules/c4m/content/c4m_content_discussion/js/document_overlay.js
+++ capacity4more/modules/c4m/content/c4m_content_discussion/js/document_overlay.js
@@ -4,24 +4,29 @@
   });
 
   $(document).on('click', function(event) {
-
     var $target = $(event.target);
-    console.log($target);
-    if ($target.is('a')) {
+    if ($target.is('a') && $target.parent().is('h2')) {
       var target = $target[0];
       var parents = $target.parents();
-      var nids = [];
-      $.each( parents, function( index, value ){
+      var elements = [];
+      $.each(parents, function(index, value) {
         var id = value.id;
+
         if (id.indexOf('node-') != -1 && id.match(/[0-9]/g)) {
-          var nid = id.replace(/\D/g, '');
-          nids.push(nid);
+          elements.push(value);
         }
       });
-      nids.reverse();
 
-      console.log(nids[0]);
-      $('#edit-c4m-related-document-und-0-target-id', parent.window.document).val('(' + nids[0] + ')');
+      elements.reverse();
+
+      var nid = elements[0].id.replace(/\D/g, '');
+      console.log(elements[0]);
+      console.log($('#' + elements[0].id, window.document).find('[property="dc:title"]'));
+
+      var title = $('#' + elements[0].id, window.document).find('[property="dc:title"]')[0];
+      title = title.co
+      console.log(title);
+      $('#edit-c4m-related-document-und-0-target-id', parent.window.document).val('(' + nid + ')');
       parent.Drupal.overlay.close();
     }
 
