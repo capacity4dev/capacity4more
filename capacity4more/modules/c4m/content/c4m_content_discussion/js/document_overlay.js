290874e45fdee0415b4979e4c59606f89d0d5397
Helena Eksler <helena@gizra.com>
2014-12-24 09:45:50 +0200

    Find nids of the albums.

--- capacity4more/modules/c4m/content/c4m_content_discussion/js/document_overlay.js
+++ capacity4more/modules/c4m/content/c4m_content_discussion/js/document_overlay.js
@@ -10,11 +10,18 @@
     if ($target.is('a')) {
       var target = $target[0];
       var parents = $target.parents();
-      console.log(parents);
-      // array_reverse(parents)
-      //find the first element with id like 'node-'+id
-      // send id to the parent document.
-      $('#edit-c4m-related-document-und-0-target-id', parent.window.document).val('we are here');
+      var nids = [];
+      $.each( parents, function( index, value ){
+        var id = value.id;
+        if (id.indexOf('node-') != -1 && id.match(/[0-9]/g)) {
+          var nid = id.replace(/\D/g, '');
+          nids.push(nid);
+        }
+      });
+      nids.reverse();
+
+      console.log(nids[0]);
+      $('#edit-c4m-related-document-und-0-target-id', parent.window.document).val('(' + nids[0] + ')');
       parent.Drupal.overlay.close();
     }
 
