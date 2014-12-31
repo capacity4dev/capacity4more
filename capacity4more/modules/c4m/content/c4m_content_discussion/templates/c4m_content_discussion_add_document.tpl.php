38659f971662f7f6fc553288c8d9f1d7bc8788e9
Helena Eksler <helena@gizra.com>
2014-12-31 11:57:13 +0200

    WIP: Clear code. Title is required.

--- capacity4more/modules/c4m/content/c4m_content_discussion/templates/c4m_content_discussion_add_document.tpl.php
+++ capacity4more/modules/c4m/content/c4m_content_discussion/templates/c4m_content_discussion_add_document.tpl.php
@@ -1,6 +1,6 @@
 <div ng-controller="DocumentCtrl">
 
-  <form name="documentForm" action="overlay-file/<?php print $file_id; ?>?render=overlay" ng-submit="createDocument($event, '<?php print $file_id; ?>', data, false)">
+  <form name="documentForm" action="overlay-file/<?php print $file_id; ?>?render=overlay" ng-submit="createDocument($event, '<?php print $file_id; ?>', data, addToLibrary)">
 
     <?php
       print('Upload new document: <br/>');
@@ -8,11 +8,12 @@
     ?>
 
     <br/>
+    <label class="control-label" for="edit-title">Title <span class="form-required" title="This field is required.">*</span></label>
     <input id="label" class="form-control" name="label" type="text" placeholder="<?php print t('Add document title'); ?>" ng-minlength=3 ng-model="data.label" required>
     <br/>
     <p>Add this document also to the group Library?</p>
-    <input type="button" id="save-edit" class="btn btn-primary" ng-click="createDocument($event, '<?php print $file_id; ?>', data, true)" tabindex="100" value="<?php print t('YES'); ?>">
-    <button type="submit" id="save" class="btn btn-primary" tabindex="100"><?php print t('NO'); ?></button>
+    <button type="submit" id="save-edit" ng-click="addToLibrary = true" class="btn btn-primary" tabindex="100"><?php print t('YES'); ?></button>
+    <button type="submit" id="save" ng-click="addToLibrary = false" class="btn btn-primary" tabindex="100"><?php print t('NO'); ?></button>
     <a href="javascript://" id="clear-button" ng-click="closeOverlay()"><?php print t('Cancel'); ?></a>
   </form>
 </div>
