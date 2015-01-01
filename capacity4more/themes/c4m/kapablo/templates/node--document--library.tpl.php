cdaecd00b2aac721352108510df320d4583499ea
Helena Eksler <helena@gizra.com>
2015-01-01 12:08:38 +0200

    Update node view.

--- capacity4more/themes/c4m/kapablo/templates/node--document--library.tpl.php
+++ capacity4more/themes/c4m/kapablo/templates/node--document--library.tpl.php
@@ -11,27 +11,22 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <div class="row">
-    <div class="col-sm-1">
-      <img src="missing_icon.png"/>
+    <div class="col-sm-3">
+      <!--Document preview-->
+      <?php print $preview; ?>
     </div>
-    <div class="col-sm-11">
-      <h2> <a href="javascript://"> <?php print $node->title; ?> </a></h2>
+    <div class="col-sm-9">
+      <div class="row">
+        <h2> <img src="missing_icon.png"/> <a href="javascript://"> <?php print $node->title; ?> </a></h2>
+      </div>
+      <div class="row">
+        <span> <?php print $document_data; ?> </span>
+      </div>
+      <div class="row">
+        <?php print $file_info; ?>
+      </div>
     </div>
-  </div>
-  <div class="row">
-    <span> <?php print $document_data; ?> </span>
   </div>
 
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
-  <div class="row">
-    <?php print $file_info; ?>
-  </div>
 
 </div>
