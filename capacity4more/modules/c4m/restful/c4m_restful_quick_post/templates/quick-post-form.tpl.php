ccf02a9d199593e892abeac27a9cdbaadbe2e831
Helena Eksler <helena@gizra.com>
2015-01-19 12:09:45 +0200

    Add button to remove the file.

--- capacity4more/modules/c4m/restful/c4m_restful_quick_post/templates/quick-post-form.tpl.php
+++ capacity4more/modules/c4m/restful/c4m_restful_quick_post/templates/quick-post-form.tpl.php
@@ -22,17 +22,16 @@
      ng-class="{ 'has-error' : errors.document }">
   <div class="form-control drop-box" ng-file-drop="onFileSelect($files);"
        ng-file-drag-over-class="file-upload-drag">
-
-    <div ng-hide="serverSide.file" class="drop-text">
+    <div class="drop-text">
       <span class="uppercase"><?php print t('Drop file here to upload'); ?></span><br/>
       <?php print t('or');?> <a href="javascript://" ng-click="browseFiles()"><?php print t('Browse') ?></a>
       <input type="file" name="document-file" id="document_file" class="hidden-input" ng-file-select="onFileSelect($files)">
     </div>
-
-    <div ng-show="serverSide.file.status == 200">
-      <div class="alert alert-success">
-        <?php print t('The document "{{ serverSide.file.data.data[0].label }}" was saved successfully.') ?>
-      </div>
+  </div>
+  <div ng-show="serverSide.file.status == 200">
+    <div class="alert alert-success">
+      <?php print t('The document "{{ serverSide.file.data.data[0].label }}" was saved successfully.') ?>
+      <button ng-click="removeFile();"><?php print t('Remove the file.'); ?></button>
     </div>
   </div>
 
