6d2db1a854475b30931f04ce36d9aa2b8aa9b0e2
Kevin Van Ransbeeck <Kevin.VanRansbeeck@amplexor.com>
2015-01-22 14:03:27 +0100

    Free tagging

--- capacity4more/themes/c4m/kapablo/js/kapablo_overlay.js
+++ capacity4more/themes/c4m/kapablo/js/kapablo_overlay.js
@@ -1,7 +1,7 @@
-if (Drupal.behaviors != 'undefined') {
-  Drupal.behaviors.verticalTabs = {
-    attach: function () {}};
-
-  Drupal.behaviors.collapse = {
-    attach: function () {}};
-}
+//if (Drupal.behaviors != 'undefined') {
+//  Drupal.behaviors.verticalTabs = {
+//    attach: function () {}};
+//
+//  Drupal.behaviors.collapse = {
+//    attach: function () {}};
+//}
