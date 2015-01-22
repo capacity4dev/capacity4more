dfc875c44cb8d1e8db4df2930a2f7b513efc0f83
Peter Decuyper <peter@serial-graphics.be>
2015-01-22 11:53:12 +0100

    Add missing config files.

--- build/travis/install/behat.sh
+++ build/travis/install/behat.sh
@@ -9,7 +9,7 @@
 cd $TRAVIS_BUILD_DIR/capacity4more/behat
 
 # Copy the travis specific behat config file.
-cp $TRAVIS_BUILD_DIR/build/travis/config/behat.local.yml ./behat.local.yml
+cp $TRAVIS_BUILD_DIR/build/travis/config/behat.local.yml.travis ./behat.local.yml
 
 # Install the behat dependencies.
 composer install --prefer-source
