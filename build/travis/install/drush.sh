f0135692292cf7f88a5dc4aa897f4e8c8b0f6155
Peter Decuyper <peter@serial-graphics.be>
2015-01-22 11:39:42 +0100

    Make sure everybody can access drush.

--- build/travis/install/drush.sh
+++ build/travis/install/drush.sh
@@ -13,6 +13,9 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 composer global require drush/drush 6.*
 phpenv rehash
 
+# Everybody should be able to use drush.
+ln -s $HOME/.composer/vendor/bin/drush /usr/bin/drush
+
 # Create the Drush alias.
 mkdir -p ~/.drush
 cp $TRAVIS_BUILD_DIR/build/travis/config/aliases.drushrc.php ~/.drush/
