690d15413c5ec16b1057b036444bbfa248f50e26
Peter Decuyper <peter@serial-graphics.be>
2015-01-22 10:31:39 +0100

    Make sure that the .drush folder exists within the travis user home folder.

--- build/travis/install/drush.sh
+++ build/travis/install/drush.sh
@@ -14,5 +14,6 @@ composer global require drush/drush 6.*
 phpenv rehash
 
 # Create the Drush alias.
+mkdir -p ~/.drush
 cp $TRAVIS_BUILD_DIR/build/travis/config/aliases.drushrc.php ~/.drush/
 composer install --prefer-source
