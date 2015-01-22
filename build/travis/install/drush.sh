412f9ec7dfdcb50f68254414a390205bffde2215
Peter Decuyper <peter@serial-graphics.be>
2015-01-22 11:33:20 +0100

    Make sure that drush can be found on the command line.

--- build/travis/install/drush.sh
+++ build/travis/install/drush.sh
@@ -16,3 +16,5 @@ phpenv rehash
 # Create the Drush alias.
 mkdir -p ~/.drush
 cp $TRAVIS_BUILD_DIR/build/travis/config/aliases.drushrc.php ~/.drush/
+
+drush --version
---
+++ build/travis/install/taskrunners.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# ---------------------------------------------------------------------------- #
+#
+# Install task runners & their dependencies.
+#
+# ---------------------------------------------------------------------------- #
+
+# Install Grunt.
+npm install -g grunt-cli
+
+# Install Bower.
+npm install -g bower
