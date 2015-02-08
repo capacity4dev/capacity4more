624e857b4a0ff5e4416c37cf2db81a51d041fbf3
Helena Eksler <helena@gizra.com>
2015-02-08 13:59:24 +0200

    Update layout.

--- capacity4more/themes/c4m/kapablo/layouts/c4m/homepage/homepage.tpl.php
+++ capacity4more/themes/c4m/kapablo/layouts/c4m/homepage/homepage.tpl.php
@@ -76,145 +76,42 @@
  */
 ?>
 
-<header id="page-header" role="banner">
-
-  <!--  USER BAR -->
-  <?php if (!empty($page['user'])): ?>
-    <section class="content-user">
-      <div class="container">
-        <div class="row">
-          <?php print render($page['user']); ?>
-        </div>
-      </div>
-    </section>
-  <?php endif; ?>
-
-
-  <div id="navbar">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-md-6 header-logo">
-          <?php if ($logo): ?>
-            <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
-            </a>
-          <?php endif; ?>
-        </div>
-        <div class="col-sm-12 col-md-6 header-search">
-          <!--          SEARCH-->
-        </div>
-      </div>
-
-<!--  BANNER    -->
-
-      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-
-        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-
-
-        <div class="navbar-collapse collapse">
-          <nav role="navigation">
-            <?php if (!empty($primary_nav)): ?>
-              <?php print render($primary_nav); ?>
-            <?php endif; ?>
-            <?php if (!empty($secondary_nav)): ?>
-              <?php print render($secondary_nav); ?>
-            <?php endif; ?>
-            <?php if (!empty($page['navigation'])): ?>
-              <?php print render($page['navigation']); ?>
-            <?php endif; ?>
-          </nav>
-        </div>
-      <?php endif; ?>
-    </div>
-  </div>
-
-  <div class="intro-block">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
-          <p><?php print t('Capacity4dev.eu is a knowledge sharing platform set-up by
-          European Commission Directorate EuropeAid. It supports the creation of
-          the Communities of the Practices in Groups where development practitioners
-          can exchange on their experience and collaborate in live.'); ?></p>
-          <input type="button" onclick="window.location.replace('user/register')" value="JOIN - SHARE - LEARN">
-        </div>
+<div class="intro-block">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <p><?php print t('Capacity4dev.eu is a knowledge sharing platform set-up by
+        European Commission Directorate EuropeAid. It supports the creation of
+        the Communities of the Practices in Groups where development practitioners
+        can exchange on their experience and collaborate in live.'); ?></p>
+        <input type="button" onclick="window.location.replace('user/register')" value="JOIN - SHARE - LEARN">
       </div>
     </div>
   </div>
-
-</header>
+</div>
 
 <div class="main-container container">
 
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header>
-  <!-- /#page-header -->
-
-  <?php if (!empty($page['content_top'])): ?>
-    <?php print render($page['content_top']); ?>
-  <?php endif; ?>
-
   <div class="row">
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-md-4" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
-
-    <section<?php print $content_column_class_kapablo; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted well"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
-
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-md-4" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
-
+    <div class="col-md-12">
+      <?php print $content['content_top']; ?>
+    </div>
   </div>
 
-  <?php if (!empty($page['content_bottom'])): ?>
-    <div class="row content-bottom">
-      <div class="col-sm-12">
-        <?php print render($page['content_bottom']); ?>
-      </div>
-    </div>
-  <?php endif; ?>
-</div>
+  <div class="row">
+    <aside class="col-md-8" role="complementary">
+      <?php print $content['sidebar_second']; ?>
+    </aside>  <!-- /#sidebar-first -->
 
-<footer class="footer container">
-  <?php print render($page['footer']); ?>
-</footer>
+    <aside class="col-md-4" role="complementary">
+      <?php print $content['sidebar_first']; ?>
+    </aside>  <!-- /#sidebar-first -->
+  </div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <?php print $content['content_bottom']; ?>
+    </div>
+  </div>
+
+</div>
