df79eb3f30db42722541d5aa74ad042b00418064
Helena Eksler <helena@gizra.com>
2015-02-12 15:33:55 +0200

    Filter is ready.

---
+++ capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/filter/filter_homepage.js
@@ -0,0 +1,13 @@
+(function ($) {
+
+  $(document).ready(function() {
+    $('#edit-homepage-filter').change(function() {
+      var radios = $('input[name="homepage-filter"]:checked');
+      var filter = radios.val();
+      window.location.search = '?filter=' + filter;
+    });
+  });
+
+})(jQuery);
+
+
