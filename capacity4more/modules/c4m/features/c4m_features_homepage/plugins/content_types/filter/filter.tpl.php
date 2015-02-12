d1e703b1d95deacc95c15ecee2a7593c5affd0d5
Helena Eksler <helena@gizra.com>
2015-02-12 12:20:48 +0200

    Change url by clicking radio.

--- capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/filter/filter.tpl.php
+++ capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/filter/filter.tpl.php
@@ -1,10 +1,12 @@
 <div class="content-filter row">
   <form class="col-sm-12">
-  <?php print t('Filter by'); ?>
-  <?php if ($is_member): ?>
-  <input type="radio" name="filter" value="groups"> <?php print t('My groups'); ?>
-  <?php endif; ?>
-  <input type="radio" name="filter" value="interests"> <?php print t('My interests'); ?>
-  <input type="radio" name="filter" value="all" checked> <?php print t('Show all'); ?>
-    </form>
+    <?php print t('Filter by'); ?>
+
+    <?php if ($is_member): ?>
+      <input type="radio" name="filter" value="groups" onclick="form.submit();"> <?php print t('My groups'); ?>
+    <?php endif; ?>
+
+    <input type="radio" name="filter" value="interests" onclick="form.submit();"> <?php print t('My interests'); ?>
+    <input type="radio" name="filter" value="all" onclick="form.submit();"> <?php print t('Show all'); ?>
+  </form>
 </div>
