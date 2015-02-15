3e9cd778c75d713c07a97448890c45aa62ce50e2
nader77 <nader@gizra.com>
2015-02-15 14:07:26 +0200

    Move carousel plugin to homepage.

--- capacity4more/modules/c4m/features/c4m_features_homepage/templates/voices-views-carousel.tpl.php
+++ capacity4more/modules/c4m/features/c4m_features_homepage/templates/voices-views-carousel.tpl.php
@@ -1,6 +1,6 @@
 <div class="row" ng-controller="CarouselCtrl">
   <div class="col-md-12">
-    <ul rn-carousel rn-carousel-index="carouselIndex" rn-carousel-controls rn-carousel-buffered class="carousel">
+    <ul ng-if="carouselIndex > 0" rn-carousel rn-carousel-index="carouselIndex" rn-carousel-controls rn-carousel-buffered class="carousel">
       <li ng-repeat="slide in carouselImages track by slide.id" ng-class="'id-' + slide.id">
         <div ng-style="{'background-image': 'url(' + slide.image + ')'}"  class="bgimage">
           <div class="intro-text">
@@ -14,6 +14,6 @@
         </div>
       </li>
     </ul>
-    <div rn-carousel-indicators ng-if="carouselImages.length > 1" slides="carouselImages" rn-carousel-index="carouselIndex"></div>
+    <div rn-carousel-indicators ng-if="carouselIndex > 1" slides="carouselImages" rn-carousel-index="carouselIndex"></div>
   </div>
 </div>
