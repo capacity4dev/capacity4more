e4e622a4555e0c1e747215c9d3851557c26d14c6
nader77 <nader@gizra.com>
2015-02-15 11:20:29 +0200

    Add pause video on hide.

--- capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/video_embed/video-embed.tpl.php
+++ capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/video_embed/video-embed.tpl.php
@@ -1,9 +1,22 @@
+<script type="text/javascript">
+  var player;
+  function onYouTubePlayerAPIReady() {
+    // create the global player from the specific iframe (#video)
+    player = new YT.Player('c4mVideo', {
+      videoId: '<?php print $video_id ?>'
+    });
+
+    jQuery('#c4mVideoModal').click(function() {
+      player.pauseVideo();
+    });
+  }
+</script>
 <div class="button-wrapper">
-  <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#c4dVideoModal">
+  <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#c4mVideoModal">
     <?php print $video_title ?>
   </button>
 </div>
-<div class="modal fade" id="c4dVideoModal" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal fade" id="c4mVideoModal" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
--- capacity4more/themes/c4m/kapablo/js/app/kapablo.js
+++ capacity4more/themes/c4m/kapablo/js/app/kapablo.js
@@ -47,5 +47,15 @@
         });
       }
     };
+
+    Drupal.behaviors.youTubeVideo = {
+      attach: function (context, settings) {
+        // Inject YouTube API script
+        var tag = document.createElement('script');
+        tag.src = "//www.youtube.com/player_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      }
+    };
   }
 )(jQuery);
