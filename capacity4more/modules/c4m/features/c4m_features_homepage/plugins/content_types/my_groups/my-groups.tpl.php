81480aa9f2cc3177030ad43a49ccda761ff14950
nader77 <nader@gizra.com>
2015-02-15 15:41:34 +0200

    Add my groups plugin.

---
+++ capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/my_groups/my-groups.tpl.php
@@ -0,0 +1,16 @@
+<div class="row">
+  <div class="col-md-12">
+    <div class="my-groups">
+      <div class="title"><?php print t('My Groups') ?></div>
+      <?php print $first_groups ?>
+      <div class="collapse" id="allGroups">
+        <div class="well">
+          <?php print $extra_groups ?>
+        </div>
+      </div>
+      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#allGroups" aria-expanded="false" aria-controls="allGroups">
+        <?php print t('Show all') ?>
+      </button>
+    </div>
+  </div>
+</div>
