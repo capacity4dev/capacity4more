51fa5520eb42c74a53ee4161ee47e7359a541be1
nader77 <nader@gizra.com>
2015-02-16 13:35:44 +0200

    Fix carousel version.

--- capacity4more/modules/c4m/features/c4m_features_homepage/templates/voices-views-carousel.tpl.php
+++ capacity4more/modules/c4m/features/c4m_features_homepage/templates/voices-views-carousel.tpl.php
@@ -1,6 +1,6 @@
 <div class="row" ng-controller="CarouselCtrl">
   <div class="col-md-12" ng-if="carouselImages.length > 0">
-    <ul rn-carousel rn-carousel-index="carouselIndex" rn-carousel-controls rn-carousel-buffered class="carousel">
+    <ul rn-carousel rn-carousel-index="carouselIndex" rn-carousel-loop rn-carousel-controls class="carousel">
       <li ng-repeat="slide in carouselImages track by slide.id" ng-class="'id-' + slide.id">
         <div ng-style="{'background-image': 'url(' + slide.image + ')'}"  class="bgimage">
           <div class="intro-text">
