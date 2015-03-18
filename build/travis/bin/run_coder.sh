611d3d26705f1ddf1d68f0a45807f471b520cd06
Peter Decuyper <github@serial-graphics.be>
2015-03-18 15:45:47 +0100

    Return an error when the coder review has detected errors.

--- build/travis/bin/run_coder.sh
+++ build/travis/bin/run_coder.sh
@@ -13,22 +13,31 @@ if [ $CODE_REVIEW != 1 ]; then
 fi
 
 PATTERNS="*.features.*,*.field_group.inc,*.strongarm.inc,*.ds.inc,*.context.inc,*.views_default.inc"
+HAS_ERRORS=0
 
 echo
 
 # Review custom modules, run each folder seperatly to avoid memory limits.
 for dir in $TRAVIS_BUILD_DIR/capacity4more/modules/c4m/*/ ; do
-    echo "Reviewing : $dir"
-    phpcs --standard=Drupal \
-      --colors \
-      -p \
-      --ignore=$PATTERNS \
-      $dir
+  echo "Reviewing : $dir"
+  phpcs --standard=Drupal \
+    --colors \
+    -p \
+    --ignore=$PATTERNS \
+    $dir
+
+  if [ $? -ne 0 ]; then
+    HAS_ERRORS=1
+  fi
 done
 
 echo
+
 
 # Review custom themes.
 # Disabled as it does not play nice with mixt environments (tpl…).
 # See https://github.com/squizlabs/PHP_CodeSniffer/issues/512
 #phpcs --standard=Drupal --colors $TRAVIS_BUILD_DIR/capacity4more/themes/c4m
+
+
+exit $HAS_ERRORS
