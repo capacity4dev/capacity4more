9dc7a1ad87cdfb3f45e2a3ab32a1d1861ff19865
nader77 <nader@gizra.com>
2015-03-29 16:24:28 +0300

    Add page detail plugin.

---
+++ capacity4more/modules/c4m/features_og/c4m_features_og_manage/plugins/content_types/group_details/group-details.tpl.php
@@ -0,0 +1,8 @@
+<div class="row">
+  <div class="col-md-12">
+    <div class="group-details panel-pane">
+      <h2 class="pane-title"><?php print t('Group details') ?></h2>
+
+    </div>
+  </div>
+</div>
