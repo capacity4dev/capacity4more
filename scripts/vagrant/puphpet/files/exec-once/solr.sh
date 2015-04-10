1c8a43880220c38a3f6e1bfdb6286486fa96f861
Maarten Segers <maarten.segers@amplexor.com>
2015-04-10 17:27:58 +0200

    restart solr

--- scripts/vagrant/puphpet/files/exec-once/solr.sh
+++ scripts/vagrant/puphpet/files/exec-once/solr.sh
@@ -9,9 +9,12 @@ sudo rsync -av search_api_solr/solr-conf/4.x/ /opt/solr/solr-4.10.2/example/solr
 # Capacity4More SOLR core.
 sudo cp -r /opt/solr/solr-4.10.2/example/solr/collection1 /opt/solr/solr-4.10.2/example/solr/capacity4more
 sudo rm -f /opt/solr/solr-4.10.2/example/solr/capacity4more/core.properties
-echo "name:capacity4more" | sudo tee --append /opt/solr/solr-4.10.2/example/solr/capacity4more/core.properties
+echo "name=capacity4more" | sudo tee --append /opt/solr/solr-4.10.2/example/solr/capacity4more/core.properties
 
 # Capacity4Dev SOLR core.
 sudo cp -r /opt/solr/solr-4.10.2/example/solr/collection1 /opt/solr/solr-4.10.2/example/solr/capacity4dev
 sudo rm -f /opt/solr/solr-4.10.2/example/solr/capacity4dev/core.properties
-echo "name:capacity4dev" | sudo tee --append /opt/solr/solr-4.10.2/example/solr/capacity4dev/core.properties
+echo "name=capacity4dev" | sudo tee --append /opt/solr/solr-4.10.2/example/solr/capacity4dev/core.properties
+
+# Restart SOLR.
+sudo /opt/solr/solr-4.10.2/bin/solr restart
