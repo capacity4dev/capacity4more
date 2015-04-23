24d1e2eac8fdbbde1c6a5e4748ba7244b4c0312e
SebastienVanRintel <sebastien.vanrintel@district01.be>
2015-04-23 09:26:56 +0200

    When no location is set in an event via quickpost, don't set a default (to Brussels)

--- capacity4more/modules/c4m/restful/c4m_restful_quick_post/components/c4m-app/src/controllers/main.js
+++ capacity4more/modules/c4m/restful/c4m_restful_quick_post/components/c4m-app/src/controllers/main.js
@@ -294,12 +294,6 @@ angular.module('c4mApp')
               }
             });
           }
-          else {
-            // Use default latitude and longitude of Brussels, Belgium.
-            submitData.location.lat = 50.850339600000000000;
-            submitData.location.lng = 4.351710300000036000;
-            submitData.location.country = 'BE';
-          }
           // Continue submitting form.
           checkForm(submitData, resource, resourceFields, type);
         });
