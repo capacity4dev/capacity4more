c6d6b02a3cc250fefd2b59b0c25ed1e0bc62fceb
Ilya Manyahin <ilya@gizra.com>
2015-04-26 11:17:50 +0300

    Fix behat test because of new policy

--- capacity4more/behat/features/bootstrap/FeatureContext/Discussion.php
+++ capacity4more/behat/features/bootstrap/FeatureContext/Discussion.php
@@ -63,10 +63,12 @@ trait Discussion {
    */
   public function aDiscussionInGroupIsCreated($title, $group_title) {
     $steps = array();
-    $steps[] = new Step\When('I visit "node/add/discussion"');
+
+    $group_node = $this->loadGroupByTitleAndType($group_title, 'group');
+
+    $steps[] = new Step\When('I visit "' . $group_node->purl . '/node/add/discussion"');
     $steps[] = new Step\When('I fill in "title" with "' . $title . '"');
     $steps[] = new Step\When('I fill in "edit-c4m-body-und-0-value" with "This is default discussion."');
-    $steps[] = new Step\When('I select "' . $group_title . '" from "edit-og-group-ref-und-0-default"');
     $steps[] = new Step\When('I press "Save"');
 
     // Check there was no error.
