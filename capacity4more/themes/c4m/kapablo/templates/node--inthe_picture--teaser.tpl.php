f867c9ee193328f48ef818b5f4fc7d93aeb9f2a2
Helena Eksler <helena@gizra.com>
2015-05-21 15:17:33 +0300

    Features. Template.

---
+++ capacity4more/themes/c4m/kapablo/templates/node--inthe_picture--teaser.tpl.php
@@ -0,0 +1,18 @@
+<div class="row carousel">
+  <div class="col-md-12">
+    <div class="inthe-picture carousel">
+      <div style="background-image: url(<?php print $file_uri; ?>);"  class="bgimage">
+        <div class="intro-text">
+          <a href="<?php print $link; ?>">
+            <span class="title">
+              <?php print render($content['c4m_heading']); ?>
+            </span>
+          </a>
+          <span class="text">
+            <?php print $text; ?>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
