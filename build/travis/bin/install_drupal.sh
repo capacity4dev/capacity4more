165d147aad9386e80b0fe4efb99b7c7d6ba5d3c3
Peter Decuyper <github@serial-graphics.be>
2015-05-21 09:28:11 +0200

    Debugging segmentation fault.

--- build/travis/bin/install_drupal.sh
+++ build/travis/bin/install_drupal.sh
@@ -22,10 +22,12 @@ cp $TRAVIS_BUILD_DIR/build/travis/config/config.sh.travis config.sh
 ./install -dy
 
 # Disable the dblog module to prevent overuse of the mysql server.
-#cd $TRAVIS_BUILD_DIR/www
-#drush -y dis dblog
+echo "Disable dblog module to avoid extra stress on MySQL server."
+cd $TRAVIS_BUILD_DIR/www
+drush -y dis dblog
 
 # Stay Calm and Clear the Cache!
+#echo "Clear all caches"
 #drush cc all --yes
 
 cd $TRAVIS_BUILD_DIR
