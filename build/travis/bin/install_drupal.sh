136a05cc5715fb3a50f7d0ed13e85008bc21c9d3
Peter Decuyper <github@serial-graphics.be>
2015-05-21 15:45:10 +0200

    Reverted debug mode on the installer.

--- build/travis/bin/install_drupal.sh
+++ build/travis/bin/install_drupal.sh
@@ -22,12 +22,12 @@ cp $TRAVIS_BUILD_DIR/build/travis/config/config.sh.travis config.sh
 ./install -dy
 
 # Disable the dblog module to prevent overuse of the mysql server.
-echo "Disable dblog module to avoid extra stress on MySQL server."
+echo "Disable dblog module to avoid extra stress on MySQL server"
 cd $TRAVIS_BUILD_DIR/www
-drush -ydv dis dblog
+drush -y dis dblog
 
 # Stay Calm and Clear the Cache!
-#echo "Clear all caches"
-#drush cc all --yes
+echo "Clear all caches"
+drush cc all --yes
 
 cd $TRAVIS_BUILD_DIR
