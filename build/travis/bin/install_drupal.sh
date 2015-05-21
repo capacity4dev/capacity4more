3c781ec0da26709e85cf4f8b3ee0bb30937622d7
Peter Decuyper <github@serial-graphics.be>
2015-05-21 11:00:55 +0200

    Enabling debug and verbose on the failing drush command.

--- build/travis/bin/install_drupal.sh
+++ build/travis/bin/install_drupal.sh
@@ -24,7 +24,7 @@ cp $TRAVIS_BUILD_DIR/build/travis/config/config.sh.travis config.sh
 # Disable the dblog module to prevent overuse of the mysql server.
 echo "Disable dblog module to avoid extra stress on MySQL server."
 cd $TRAVIS_BUILD_DIR/www
-drush -y dis dblog
+drush -ydv dis dblog
 
 # Stay Calm and Clear the Cache!
 #echo "Clear all caches"
