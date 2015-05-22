bfcd9e4bdbcbe42194336d890ccb55ece725f526
Peter Decuyper <github@serial-graphics.be>
2015-05-22 12:28:51 +0200

    Refactored statistics info hook + refactored the code to get the stats info & collect the stats data.

--- capacity4more/modules/c4m/content/c4m_content/c4m_content.api.php
+++ capacity4more/modules/c4m/content/c4m_content/c4m_content.api.php
@@ -16,55 +16,74 @@
  * what statistics should be made available.
  *
  * @return array
- *   An array of statistic lists and their settings.
+ *   An array of statistic lists and their settings. Grouped by the scope where
+ *   the statistics can be collected.
+ *
+ *   The supported scopes are:
+ *   - global : Overall statistics.
+ *   - group : Statistics for a specific group.
+ *
  *   Each list has the following data:
- *   - type : The identifier of the statistics list.
- *   - entity_type : The entity type of the statistics.
- *   - bundles : The bundles to use in the statistics when it's a node as entity type.
- *   - singular : The singular name of the statistic type.
- *   - plural : The plural name of the statistic type.
- *   - state : The status of the entity as integer (published, unpublished, ...).
- *   - c4m_status : The C4M status of the entity (published, requested, draft, ...).
- *   - scope : Sets the list as global/group statistics info.
- *   - aggregate : Array containing type.
- *   - aggregate type : The identifier to group lists, belonging to this type, together.
- *   - weight : The weight used to sort the lists.
- *   - feature_name : The name of the group feature.
- *   - attributes : Standard attributes array to use in theme function (class, id, ...).
+ *   - type:
+ *       The identifier of the statistics list.
+ *   - entity_type:
+ *       The entity type of the statistics.
+ *   - bundles:
+ *       The bundles to use in the statistics when it's a node as entity type.
+ *   - singular:
+ *       The singular name of the statistic type.
+ *   - plural:
+ *       The plural name of the statistic type.
+ *   - state:
+ *       The status of the entity as integer (published, unpublished).
+ *   - c4m_status:
+ *       The C4M status of the entity (published, requested, ...).
+ *   - aggregate:
+ *       Array containing type.
+ *   - aggregate type:
+ *       The identifier to group lists, belonging to this type, together.
+ *   - weight:
+ *       The weight used to sort the lists.
+ *   - feature_name:
+ *       The name of the group feature.
+ *   - attributes:
+ *       Standard attributes array to use in theme function (class, id, ...).
  */
 function hook_c4m_content_statistics_info() {
   return array(
-    'c4m_discussions' => array(
-      'type'        => 'discussion',
-      'entity_type' => 'node',
-      'bundles'     => array('discussion'),
-      'singular'    => 'Post',
-      'plural'      => 'Posts',
-      'state'       => NULL,
-      'c4m_status'  => NULL,
-      'scope'       => 'global',
-      'aggregate'   => array(
-        'type' => 'posts',
-      ),
-      'weight'      => 1,
-      'attributes'  => array(
-        'class' => array('posts'),
+    'global' => array(
+      'c4m_discussions' => array(
+        'type'        => 'discussion',
+        'entity_type' => 'node',
+        'bundles'     => array('discussion'),
+        'singular'    => 'Post',
+        'plural'      => 'Posts',
+        'state'       => NULL,
+        'c4m_status'  => NULL,
+        'aggregate'   => array(
+          'type' => 'posts',
+        ),
+        'weight'      => 1,
+        'attributes'  => array(
+          'class' => array('posts'),
+        ),
       ),
     ),
-    'c4m_og_discussions' => array(
-      'type'          => 'discussion',
-      'entity_type'   => 'node',
-      'bundles'       => array('discussion'),
-      'singular'      => 'Discussion',
-      'plural'        => 'Discussions',
-      'state'         => NULL,
-      'c4m_status'    => array('published', 'archived'),
-      'scope'         => 'group',
-      'aggregate'     => array(),
-      'weight'        => 1,
-      'feature_name'  => 'c4m_features_og_discussions',
-      'attributes'    => array(
-        'class' => array('og-discussions'),
+    'group' => array(
+      'c4m_og_discussions' => array(
+        'type'          => 'discussion',
+        'entity_type'   => 'node',
+        'bundles'       => array('discussion'),
+        'singular'      => 'Discussion',
+        'plural'        => 'Discussions',
+        'state'         => NULL,
+        'c4m_status'    => array('published', 'archived'),
+        'aggregate'     => array(),
+        'weight'        => 1,
+        'feature_name'  => 'c4m_features_og_discussions',
+        'attributes'    => array(
+          'class' => array('og-discussions'),
+        ),
       ),
     ),
   );
