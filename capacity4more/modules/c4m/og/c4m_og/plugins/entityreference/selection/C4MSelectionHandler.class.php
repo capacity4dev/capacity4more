aa0458df497c4058bde4c1883db7f047d4a99f68
Helena Eksler <helena@gizra.com>
2015-05-26 15:16:39 +0300

    Remove duplication.

--- capacity4more/modules/c4m/og/c4m_og/plugins/entityreference/selection/C4MSelectionHandler.class.php
+++ capacity4more/modules/c4m/og/c4m_og/plugins/entityreference/selection/C4MSelectionHandler.class.php
@@ -7,7 +7,7 @@
 /**
  * OG selection handler.
  */
-class C4MSelectionHandler extends OgSelectionHandler {
+class C4MSelectionHandler extends C4MOgSelectionHandler {
   /**
    * {@inheritdoc}
    */
@@ -19,12 +19,6 @@ class C4MSelectionHandler extends OgSelectionHandler {
    * {@inheritdoc}
    */
   public function validateReferencableEntities(array $ids) {
-    // Remove empty ids.
-    foreach ($ids as $key => $value) {
-      if ($value === NULL) {
-        unset($ids[$key]);
-      }
-    }
     return parent::validateReferencableEntities($ids);
   }
 
@@ -32,62 +26,7 @@ class C4MSelectionHandler extends OgSelectionHandler {
    * {@inheritdoc}
    */
   public function buildEntityFieldQuery($match = NULL, $match_operator = 'CONTAINS') {
-    parent::buildEntityFieldQuery();
-
-    global $user;
-
-    $handler = EntityReference_SelectionHandler_Generic::getInstance($this->field, $this->instance, $this->entity_type, $this->entity);
-    $query = $handler->buildEntityFieldQuery($match, $match_operator);
-
-    // FIXME: http://drupal.org/node/1325628
-    unset($query->tags['node_access']);
-
-    // FIXME: drupal.org/node/1413108
-    unset($query->tags['entityreference']);
-
-    $query->addTag('entity_field_access');
-    $query->addTag('og');
-
-    $group_type = $this->field['settings']['target_type'];
-    $entity_info = entity_get_info($group_type);
-
-    if (!field_info_field(OG_GROUP_FIELD)) {
-      // There are no groups, so falsify query.
-      $query->propertyCondition($entity_info['entity keys']['id'], -1, '=');
-      return $query;
-    }
-
-    // Show only the entities that are active groups.
-    $query->fieldCondition(OG_GROUP_FIELD, 'value', 1, '=');
-
-    $account = user_load($user->uid);
-    if (user_access('administer site configuration', $account)) {
-      // Site administrator can choose also groups he is not member of.
-      $query->fieldCondition('c4m_og_status', 'value', array('deleted'), 'NOT IN');
-      return $query;
-    }
-
-    $user_groups = og_get_groups_by_user(NULL, $group_type);
-    $user_groups = $user_groups ? $user_groups : array();
-
-    if ($user_groups) {
-      $query->propertyCondition($entity_info['entity keys']['id'], $user_groups, 'IN');
-    }
-    else {
-      // User doesn't have permission to select any group so falsify this
-      // query.
-      $query->propertyCondition($entity_info['entity keys']['id'], -1, '=');
-    }
-
-    $unallowed_values = array(
-      'requested',
-      'archived',
-      'rejected',
-      'deleted',
-    );
-    $query->fieldCondition('c4m_og_status', 'value', $unallowed_values, 'NOT IN');
-
-    return $query;
+    return parent::buildEntityFieldQuery();
   }
 
 }
