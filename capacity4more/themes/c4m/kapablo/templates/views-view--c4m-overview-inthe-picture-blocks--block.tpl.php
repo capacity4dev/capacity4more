36ed6a260a3fedd41c0ba16423cecb305ba8bc7b
nader77 <nader@gizra.com>
2015-05-28 14:15:57 +0300

    Add href on the slide.

--- capacity4more/themes/c4m/kapablo/templates/views-view--c4m-overview-inthe-picture-blocks--block.tpl.php
+++ capacity4more/themes/c4m/kapablo/templates/views-view--c4m-overview-inthe-picture-blocks--block.tpl.php
@@ -13,18 +13,18 @@
   <div class="col-md-12" ng-if="carouselImages.length > 1">
     <ul rn-carousel rn-carousel-index="carouselIndex" rn-carousel-loop rn-carousel-controls class="carousel">
       <li ng-repeat="slide in carouselImages track by slide.id" ng-class="'id-' + slide.id">
-        <div ng-style="{'background-image': 'url(' + slide.image + ')'}"  class="bgimage">
-          <div class="intro-text" ng-if="slide.title && slide.text">
-            <a href="{{ slide.alias }}">
+        <a ng-href="{{ slide.alias }}">
+          <div ng-style="{'background-image': 'url(' + slide.image + ')'}"  class="bgimage">
+            <div class="intro-text" ng-if="slide.title || slide.text">
               <span class="title">
                 {{ slide.title }}
               </span>
-            </a>
-            <span class="text">
-              {{ slide.text }}
-            </span>
+              <span class="text">
+                {{ slide.text }}
+              </span>
+            </div>
           </div>
-        </div>
+        </a>
       </li>
     </ul>
     <div rn-carousel-indicators ng-if="carouselImages.length > 1" slides="carouselImages" rn-carousel-index="carouselIndex"></div>
@@ -32,18 +32,18 @@
   <div class="col-md-12" ng-if="carouselImages.length == 1">
     <ul class="no-carousel carousel">
       <li ng-repeat="slide in carouselImages track by slide.id" ng-class="'id-' + slide.id" class="one-slide">
-        <div ng-style="{'background-image': 'url(' + slide.image + ')'}"  class="bgimage">
-          <div class="intro-text" ng-if="slide.title && slide.text">
-            <a href="{{ slide.alias }}">
+        <a ng-href="{{ slide.alias }}">
+          <div ng-style="{'background-image': 'url(' + slide.image + ')'}"  class="bgimage">
+            <div class="intro-text" ng-if="slide.title || slide.text">
               <span class="title">
                 {{ slide.title }}
               </span>
-            </a>
-            <span class="text">
-              {{ slide.text }}
-            </span>
+              <span class="text">
+                {{ slide.text }}
+              </span>
+            </div>
           </div>
-        </div>
+        </a>
       </li>
     </ul>
   </div>
