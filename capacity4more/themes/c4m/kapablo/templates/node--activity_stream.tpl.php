cb615be3ae2b28d4e224eed03ad39c4417095c4c
Peter Decuyper <github@serial-graphics.be>
2015-06-23 13:49:04 +0200

    Fixed code style issues.

--- capacity4more/themes/c4m/kapablo/templates/node--activity_stream.tpl.php
+++ capacity4more/themes/c4m/kapablo/templates/node--activity_stream.tpl.php
@@ -1,21 +1,18 @@
 <?php
-
 /**
  * @file
  * Display a node in "Activity stream" view mode.
  *
  * @see node.tpl.php
  */
+
+// We hide the comments and links now so that we can render them later.
+hide($content['comments']);
+hide($content['links']);
 ?>
+
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
   <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
+    <?php print render($content); ?>
   </div>
-
 </div>
