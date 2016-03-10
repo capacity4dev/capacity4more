2181f44b7695abc692b007ca6f1a786fa1e719d7
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-10 13:29:06 +0100

    DRULETON: Move Angular c4m-app to the skeleton building structure

---
+++ config/bin/src/angular_build.sh
@@ -0,0 +1,60 @@
+################################################################################
+# Functionality to build the theme using Grunt.
+################################################################################
+
+##
+# Build the Angular application.
+#
+# Params:
+# string $1
+#   the grunt task to build.
+##
+function angular_build {
+  angular_build_npm
+  angular_build_grunt $1
+  angular_bower_install
+}
+
+##
+# Install/update the NodeJS modules.
+#
+# The modules will only be installed if they are not installed yet.
+# The modules will only be updated if the --no-update option is not passed.
+##
+function angular_build_npm {
+  cd "$DIR_PROJECT/profiles/capacity4more/modules/c4m/restful/c4m_restful_quick_post/components/c4m-app"
+
+  if [ ! -d "./node_modules" ]; then
+    npm install
+  elif [ $( option_is_set "--no-update" ) -ne 1 ]; then
+    npm install
+  fi
+
+  cd "$DIR_ROOT"
+}
+
+##
+# Run the Grunt task(s).
+#
+# string $1
+#   the grunt task to build.
+##
+function angular_build_grunt {
+  cd "$DIR_PROJECT/profiles/capacity4more/modules/c4m/restful/c4m_restful_quick_post/components/c4m-app"
+
+  node_modules/grunt-cli/bin/grunt $1
+
+  cd "$DIR_ROOT"
+}
+
+##
+# Install bower dependencies
+##
+function angular_bower_install {
+  cd "$DIR_ROOT"
+
+  bower cache clean
+  bower install $DIR_PROJECT/profiles/capacity4more/modules/c4m/restful/c4m_restful_quick_post/components/c4m-app
+
+  cd "$DIR_ROOT"
+}
--- config/install/drupal_install_after.sh
+++ config/install/drupal_install_after.sh
@@ -3,3 +3,9 @@ markup_h1 "Build the Theme."
 source "$DIR_CONFIG_SRC/theme_build.sh"
 theme_build build
 echo
+
+# Build the Angular app.
+markup_h1 "Build the Angular app."
+source "$DIR_CONFIG_SRC/angular_build.sh"
+angular_build build
+echo
