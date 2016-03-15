b09b1ad265fe7c1db024b6ef3c33076ed3319eaf
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-15 11:05:27 +0100

    Create dummy content migration command

---
+++ config/bin/src/dummy.sh
@@ -0,0 +1,25 @@
+################################################################################
+# Functionality to migrate the dummy content.
+################################################################################
+
+##
+# Migrate dummy content.
+##
+function dummy_content {
+  dummy_content_modules
+  dummy_content_migrate
+}
+
+##
+# Enable migration modules.
+##
+function dummy_content_modules {
+  drupal_drush --uri="$SITE_URL" en -y migrate migrate_ui c4m_demo_content
+}
+
+##
+# Execute dummy migration.
+##
+function dummy_content_migrate {
+  drupal_drush --uri="$SITE_URL" mi --force --update --group=c4m_demo_content
+}
