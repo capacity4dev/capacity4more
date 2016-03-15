36dce23cbeabaf8fcd5c98b61004f229c23b3f63
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-15 14:18:32 +0100

    TRAVIS: Install MySQL

---
+++ ci/bin/install_mysql.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+# ---------------------------------------------------------------------------- #
+#
+# Installs && configure MySQL server & database(s).
+#
+# ---------------------------------------------------------------------------- #
+
+
+# No need for special config if the profile is not installed.
+if [ $INSTALL_PROFILE != 1 ]; then
+ exit 0;
+fi
+
+
+# Copy the proper config file.
+sudo cp -f $TRAVIS_BUILD_DIR/ci/config/drupal.cnf /etc/mysql/conf.d/drupal.cnf
+
+# Restart the mysql service.
+sudo service mysql restart
