8b0dd93247c93423bf2e77eebade2260ad559d4f
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-15 14:17:14 +0100

    TRAVIS: Sudo php-fpm

--- ci/bin/install_apache.sh
+++ ci/bin/install_apache.sh
@@ -23,7 +23,7 @@ sudo a2enmod rewrite actions fastcgi alias
 # Config php-fpm.
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+sudo ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
 # Set sendmail so php doesn't throw an error while trying to send out email.
 echo "sendmail_path='true'" >> `php --ini | grep "Loaded Configuration" | awk '{print $4}'`
@@ -34,7 +34,6 @@ mkdir $TRAVIS_BUILD_DIR/web
 # Create the default vhost config file.
 sudo cp -f $TRAVIS_BUILD_DIR/ci/config/apache.conf /etc/apache2/sites-available/default
 sudo sed -e "s?%TRAVIS_BUILD_DIR%?$TRAVIS_BUILD_DIR?g" --in-place /etc/apache2/sites-available/default
-sudo service apache2 restart
 
 # Restart Apache
 sudo service apache2 restart
