40a02eba964a65181f7de61d57bcb79ee485abda
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-15 15:12:35 +0100

    TRAVIS: Prepare behat

---
+++ ci/bin/install_behat.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+# ---------------------------------------------------------------------------- #
+#
+# Install Behat dependencies.
+#
+# ---------------------------------------------------------------------------- #
+
+
+# Behat has no use if the profile is not installed.
+if [ $INSTALL_PROFILE != 1 ]; then
+ exit 0;
+fi
+
+# Check first if we need behat.
+if [ "$BEHAT_TAG" = "" ]; then
+  exit 0;
+fi
+
+
+cd $TRAVIS_BUILD_DIR/capacity4more/behat
+
+# Copy the travis specific behat config file.
+cp $TRAVIS_BUILD_DIR/build/travis/config/behat.local.yml.travis ./behat.local.yml
+
+# Install the behat dependencies.
+composer install --prefer-source
+
+cd $TRAVIS_BUILD_DIR
