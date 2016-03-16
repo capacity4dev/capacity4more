0c963b1666b44f0279c03fc3fbdd81e70641c4e9
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-16 14:16:54 +0100

    TRAVIS: Migrate module was not downloaded for prd environment, we need a fixed version.

--- config/bin/src/dummy.sh
+++ config/bin/src/dummy.sh
@@ -14,10 +14,8 @@ function dummy_content {
 # Enable migration modules.
 ##
 function dummy_content_modules {
+  drupal_drush --uri="$SITE_URL" en -y migrate c4m_demo_content
   drupal_drush --uri="$SITE_URL" dis -y search_api_attachments
-  drupal_drush --uri="$SITE_URL" en -y migrate
-  drupal_drush --uri="$SITE_URL" en -y migrate_extras
-  drupal_drush --uri="$SITE_URL" en -y c4m_demo_content
 }
 
 ##
