f34c94e303b7844e65f03f8e7b1ec3fbaa41697d
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-17 10:10:54 +0100

    Install behat and selenium browser

--- ci/bin/install_behat.sh
+++ ci/bin/install_behat.sh
@@ -19,10 +19,10 @@ if [ "$BEHAT_TAG" = "" ]; then
 fi
 
 
-cd $TRAVIS_BUILD_DIR/capacity4more/behat
+cd $TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat
 
 # Copy the travis specific behat config file.
-cp $TRAVIS_BUILD_DIR/build/travis/config/behat.local.yml.travis ./behat.local.yml
+cp $TRAVIS_BUILD_DIR/ci/config/behat.local.yml.travis ./behat.local.yml
 
 # Install the behat dependencies.
 composer install --prefer-source
--- ci/bin/install_browser.sh
+++ ci/bin/install_browser.sh
@@ -13,7 +13,7 @@ if [ $INSTALL_PROFILE -ne 1 ]; then
  exit 0;
 fi
 
-# Check first if we realy need this.
+# Check first if we really need this.
 if [ "$BEHAT_TAG" -ne "javascript" ]; then
   exit 0
 fi
@@ -25,9 +25,9 @@ sh -e /etc/init.d/xvfb start
 sleep 3
 
 # Run selenium.
-cd $TRAVIS_BUILD_DIR/capacity4more/behat
-wget http://selenium-release.storage.googleapis.com/2.40/selenium-server-standalone-2.40.0.jar
-java -jar selenium-server-standalone-2.40.0.jar -p 4444 &
+cd $TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat
+wget http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar
+java -jar selenium-server-standalone-2.53.0.jar -p 4444 &
 sleep 5
 
 # Run phantomJs.
