bcf37c353a7671536abea0e52b0a838ecba33674
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-18 17:02:22 +0100

    Add header images and imagemin grunt task

--- project/profiles/capacity4more/themes/c4m/kapablo/build/Gruntfile.js
+++ project/profiles/capacity4more/themes/c4m/kapablo/build/Gruntfile.js
@@ -4,209 +4,236 @@
  */
 
 module.exports = function (grunt) {
-  grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-      /**
-       * Set project object.
-       *
-       * Variables:
-       * <%= project.src %> : The root path of the project.
-       */
-      project: {
-          src: 'src',
-          dst: '..'
+        /**
+         * Set project object.
+         *
+         * Variables:
+         * <%= project.src %> : The root path of the project.
+         */
+        project: {
+            src: 'src',
+            dst: '..'
         },
 
-      bootstrapDir: './node_modules/bootstrap-sass',
+        bootstrapDir: './node_modules/bootstrap-sass',
 
-      // Compile SASS, including bootstrap-sass.
-      sass: {
-          dev: {
-              options: {
-                  style: 'expanded',
-                  sourcemap: 'auto',
-                  debug: true,
-                  loadPath: '<%= bootstrapDir %>/assets/stylesheets'
+        // Compile SASS, including bootstrap-sass.
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                    sourcemap: 'auto',
+                    debug: true,
+                    loadPath: '<%= bootstrapDir %>/assets/stylesheets'
                 },
-              files: [{
-                  expand: true,
-                  cwd: '<%= project.src %>/stylesheets',
-                  src: ['*.scss'],
-                  dest: '<%= project.dst %>/css',
-                  ext: '.concat.css'
+                files: [{
+                    expand: true,
+                    cwd: '<%= project.src %>/stylesheets',
+                    src: ['*.scss'],
+                    dest: '<%= project.dst %>/css',
+                    ext: '.concat.css'
                 }]
             },
-          build: {
-              options: {
-                  style: 'compressed',
-                  sourcemap: 'none',
-                  debug: false,
-                  loadPath: '<%= bootstrapDir %>/assets/stylesheets'
+            build: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none',
+                    debug: false,
+                    loadPath: '<%= bootstrapDir %>/assets/stylesheets'
                 },
-              files: [{
-                  expand: true,
-                  cwd: '<%= project.src %>/stylesheets',
-                  src: ['*.scss'],
-                  dest: '<%= project.dst %>/css',
-                  ext: '.concat.css'
+                files: [{
+                    expand: true,
+                    cwd: '<%= project.src %>/stylesheets',
+                    src: ['*.scss'],
+                    dest: '<%= project.dst %>/css',
+                    ext: '.concat.css'
                 }]
             }
         },
 
-      // Auto prefix CSS files with specified browser support.
-      autoprefixer: {
-          dev: {
-              options: {
-                  browsers: ['last 2 versions', 'ie 9'],
-                  diff: true,
-                  map: true
+        // Auto prefix CSS files with specified browser support.
+        autoprefixer: {
+            dev: {
+                options: {
+                    browsers: ['last 2 versions', 'ie 9'],
+                    diff: true,
+                    map: true
                 },
-              files: [{
-                  expand: true,
-                  cwd: '<%= project.dst %>/css',
-                  src: ['*.concat.css'],
-                  dest: '<%= project.dst %>/css',
-                  ext: '.concat.css'
+                files: [{
+                    expand: true,
+                    cwd: '<%= project.dst %>/css',
+                    src: ['*.concat.css'],
+                    dest: '<%= project.dst %>/css',
+                    ext: '.concat.css'
                 }]
             },
-          build: {
-              options: {
-                  browsers: ['last 2 versions', 'ie 6'],
-                  diff: false,
-                  map: false
+            build: {
+                options: {
+                    browsers: ['last 2 versions', 'ie 6'],
+                    diff: false,
+                    map: false
                 },
-              files: [{
-                  expand: true,
-                  cwd: '<%= project.dst %>/css',
-                  src: ['*.concat.css'],
-                  dest: '<%= project.dst %>/css',
-                  ext: '.concat.css'
+                files: [{
+                    expand: true,
+                    cwd: '<%= project.dst %>/css',
+                    src: ['*.concat.css'],
+                    dest: '<%= project.dst %>/css',
+                    ext: '.concat.css'
                 }]
             }
         },
 
-      // ESLint Theme's JavaScript file.
-      eslint: {
-          src: ['<%= project.src %>/javascripts/kapablo.js']
+        // ESLint Theme's JavaScript file.
+        eslint: {
+            src: ['<%= project.src %>/javascripts/kapablo.js']
         },
 
-      // Concatenate JavaScript files.
-      concat: {
-          dev: {
-              options: {
-                  sourceMap: true
+        // Concatenate JavaScript files.
+        concat: {
+            dev: {
+                options: {
+                    sourceMap: true
                 },
-              files: {
-                  '<%= project.dst %>/js/kapablo.concat.js': [
-                      '<%= project.src %>/javascripts/kapablo.js'
-                  ],
-                  '<%= project.dst %>/js/bootstrap.concat.js': [
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/affix.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/alert.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/button.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/carousel.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/collapse.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/dropdown.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/modal.js',.
-                       '<%= bootstrapDir %>/assets/javascripts/bootstrap/tooltip.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/popover.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/scrollspy.js',.
-                       '<%= bootstrapDir %>/assets/javascripts/bootstrap/tab.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/transition.js'.
-                  ]
+                files: {
+                    '<%= project.dst %>/js/kapablo.concat.js': [
+                        '<%= project.src %>/javascripts/kapablo.js'
+                    ],
+                    '<%= project.dst %>/js/bootstrap.concat.js': [
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/affix.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/alert.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/button.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/carousel.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/collapse.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/dropdown.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/modal.js',.
+                        '<%= bootstrapDir %>/assets/javascripts/bootstrap/tooltip.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/popover.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/scrollspy.js',.
+                        '<%= bootstrapDir %>/assets/javascripts/bootstrap/tab.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/transition.js'.
+                    ]
                 }
             },
-          build: {
-              options: {
-                  sourceMap: false,
-                  stripBanners: true
+            build: {
+                options: {
+                    sourceMap: false,
+                    stripBanners: true
                 },
-              files: {
-                  '<%= project.dst %>/js/kapablo.concat.js': [
-                      '<%= project.src %>/javascripts/kapablo.js'
-                  ],
-                  '<%= project.dst %>/js/bootstrap.concat.js': [
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/affix.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/alert.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/button.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/carousel.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/collapse.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/dropdown.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/modal.js',.
-                       '<%= bootstrapDir %>/assets/javascripts/bootstrap/tooltip.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/popover.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/scrollspy.js',.
-                       '<%= bootstrapDir %>/assets/javascripts/bootstrap/tab.js',
-                      // '<%= bootstrapDir %>/assets/javascripts/bootstrap/transition.js'.
-                  ]
+                files: {
+                    '<%= project.dst %>/js/kapablo.concat.js': [
+                        '<%= project.src %>/javascripts/kapablo.js'
+                    ],
+                    '<%= project.dst %>/js/bootstrap.concat.js': [
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/affix.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/alert.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/button.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/carousel.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/collapse.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/dropdown.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/modal.js',.
+                        '<%= bootstrapDir %>/assets/javascripts/bootstrap/tooltip.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/popover.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/scrollspy.js',.
+                        '<%= bootstrapDir %>/assets/javascripts/bootstrap/tab.js',
+                        // '<%= bootstrapDir %>/assets/javascripts/bootstrap/transition.js'.
+                    ]
                 }
             }
         },
 
-      // Uglify JavaScript files.
-      uglify: {
-          build: {
-              options: {
-                  report: 'min'
+        // Uglify JavaScript files.
+        uglify: {
+            build: {
+                options: {
+                    report: 'min'
                 },
-              files: [{
-                  expand: true,
-                  cwd: '<%= project.dst %>/js',
-                  src: ['*.concat.js'],
-                  dest: '<%= project.dst %>/js',
-                  ext: '.concat.js'
+                files: [{
+                    expand: true,
+                    cwd: '<%= project.dst %>/js',
+                    src: ['*.concat.js'],
+                    dest: '<%= project.dst %>/js',
+                    ext: '.concat.js'
                 }]
             }
         },
 
-      // Remove destination files.
-      clean: {
-          options: {
-              force: true
-            },
-          dst: [
-              "<%= project.dst %>/css",
-              "<%= project.dst %>/js",
-              "<%= project.dst %>/images"
-          ],
+        // Minify images.
+        imagemin: {
+            build: {
+                options: {
+                    optimizationLevel: 3,
+                    svgoPlugins: [{removeViewBox: false}],
+                    progressive: true,
+                    interlaced: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= project.src %>/images',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: '<%= project.dst %>/images',
+                }]
+            }
         },
 
-      // Automate some tasks during development (if files change).
-      watch: {
-          style: {
-              files: [
-                  '<%= project.src %>/stylesheets/*.scss',
-                  '<%= project.src %>/stylesheets/**/*.scss'
-              ],
-              tasks: ['sass:dev', 'autoprefixer:dev'],
-              options: {
-                  livereload: true
+        // Remove destination files.
+        clean: {
+            options: {
+                force: true
+            },
+            dst: [
+                "<%= project.dst %>/css",
+                "<%= project.dst %>/js",
+                "<%= project.dst %>/images"
+            ],
+        },
+
+        // Automate some tasks during development (if files change).
+        watch: {
+            style: {
+                files: [
+                    '<%= project.src %>/stylesheets/**/*.scss'
+                ],
+                tasks: ['sass:dev', 'autoprefixer:dev'],
+                options: {
+                    livereload: true
                 }
             },
 
-          scripts: {
-              files: [
-                  '<%= project.src %>/javascripts/*.js',
-                  '<%= project.src %>/javascripts/**/*.js'
-              ],
-              tasks: ['eslint', 'concat', 'uglify'],
-              options: {
-                  livereload: true
+            scripts: {
+                files: [
+                    '<%= project.src %>/javascripts/**/*.js'
+                ],
+                tasks: ['eslint', 'concat', 'uglify'],
+                options: {
+                    livereload: true
+                }
+            },
+
+            images: {
+                files: [
+                    '<%= project.src %>/images/**/*.{png,jpg,gif}'
+                ],
+                tasks: ['imagemin'],
+                options: {
+                    livereload: true
                 }
             }
         }
     });
 
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-eslint');
 
-  grunt.registerTask('dev', [
+    grunt.registerTask('dev', [
         'clean',
 
         'sass:dev',
@@ -216,7 +243,7 @@ module.exports = function (grunt) {
         'concat:dev'
     ]);
 
-  grunt.registerTask('build', [
+    grunt.registerTask('build', [
         'clean',
 
         'sass:build',
@@ -227,7 +254,7 @@ module.exports = function (grunt) {
         'uglify'
     ]);
 
-  grunt.registerTask('default', [
+    grunt.registerTask('default', [
         'build'
     ]);
 }
