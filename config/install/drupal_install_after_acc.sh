ed66989b0671bbf0a87483f2a37b6b781b3463f5
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-29 14:49:48 +0200

    DRULETON: Unneeded after files

--- config/install/drupal_install_after_acc.sh
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Use this file to add custom script steps that should run after the
-# drupal installation has finished.
-################################################################################
-
-
-source "$DIR_CONFIG/install/drupal_install_after_prd.sh"
