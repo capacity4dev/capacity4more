12313beb1ad4edea94a17e441d327c02f162aca6
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-29 10:22:18 +0200

    DRULETON: Add memcache config to prd and acc

---
+++ config/install/drupal_install_after_acc.sh
@@ -0,0 +1,7 @@
+################################################################################
+# Use this file to add custom script steps that should run after the
+# drupal installation has finished.
+################################################################################
+
+
+source "$DIR_CONFIG/install/drupal_install_after_prd.sh"
