8c2fe11934780123c68a43610b01ea81fcceb80d
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-04-08 11:02:21 +0200

    Create/configure calendar icon with date inside of it

--- project/profiles/capacity4more/modules/c4m/ds/c4m_ds/layouts/c4m_ds_3_9_fluid_stacked/c4m-ds-3-9-fluid-stacked.tpl.php
+++ project/profiles/capacity4more/modules/c4m/ds/c4m_ds/layouts/c4m_ds_3_9_fluid_stacked/c4m-ds-3-9-fluid-stacked.tpl.php
@@ -18,10 +18,10 @@ print $layout_attributes; ?> class="<?php print $classes; ?>">
     </<?php print $header_wrapper; ?>>
   </div>
   <div class="row">
-    <<?php print $left_wrapper; ?> class="group-left col-sm-3 <?php print $left_classes; ?>">
+    <<?php print $left_wrapper; ?> class="group-left col-xs-3 <?php print $left_classes; ?>">
       <?php print $left; ?>
     </<?php print $left_wrapper; ?>>
-    <<?php print $right_wrapper; ?> class="group-right col-sm-9 <?php print $right_classes; ?>">
+    <<?php print $right_wrapper; ?> class="group-right col-xs-9 <?php print $right_classes; ?>">
       <?php print $right; ?>
     </<?php print $right_wrapper; ?>>
   </div>
