d4f205984cd262f90f69b62608180b157f55471e
Mircea Fernea <mircea.fernea@amplexor.com>
2016-04-15 13:55:22 +0300

    CFM-701: Improve captcha error display.

--- project/profiles/capacity4more/modules/c4m/helper/c4m_helper_captcha/js/c4m_helper_captcha.js
+++ project/profiles/capacity4more/modules/c4m/helper/c4m_helper_captcha/js/c4m_helper_captcha.js
@@ -6,13 +6,22 @@
 (function ($) {
   Drupal.behaviors.c4m_captcha = {
     attach: function(context) {
+      $('.captcha').bind('DOMSubtreeModified', function() {
+        $('#security_code', this).addClass('form-control');
+      });
       var arrayLength = Drupal.settings.c4m_captcha_forms.length;
       for (var i = 0; i < arrayLength; i++) {
         var formId = '#' + Drupal.settings.c4m_captcha_forms[i].replace(/_/g, '-');
         $(formId, context).submit(function(event) {
+          var $captcha_error = $('.captcha_error', this);
+
           // @todo Make sure to allow multiple captcha forms in one page.
           if (!captcha.captchaObjects[0].validateCaptcha()) {
+            $captcha_error.removeClass('hidden').prev().addClass('has-error');
             event.preventDefault();
+          }
+          else {
+            $captcha_error.addClass('hidden').prev().addClass('has-error');
           }
         });
       }
