f40e610584ef8af2b256cf5aaf9cba5770b23946
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-04-18 09:49:24 +0200

    Replace getlocations field base/instance with addressfield + geofield, add leaflet etc...

--- config/make/fields.make
+++ config/make/fields.make
@@ -37,12 +37,6 @@ projects[link][version] = "1.4"
 projects[money][subdir] = "contrib"
 projects[money][version] = "1.x-dev"
 
-projects[getlocations][subdir] = "contrib"
-projects[getlocations][version] = "1.17"
-
-projects[location][subdir] = "contrib"
-projects[location][version] = "3.7"
-
 projects[reldate][subdir] = "contrib"
 projects[reldate][version] = "1.x-dev"
 
@@ -53,8 +47,3 @@ libraries[leaflet][download][type]= "get"
 libraries[leaflet][download][url] = "https://leafletjs-cdn.s3.amazonaws.com/content/leaflet/master/leaflet.zip"
 libraries[leaflet][directory_name] = "leaflet"
 libraries[leaflet][destination] = "libraries"
-
-libraries[getlocations_markers][download][type]= "get"
-libraries[getlocations_markers][download][url] = "https://dl.dropboxusercontent.com/u/41489105/Drupal/getlocations/getlocations-markers.zip"
-libraries[getlocations_markers][directory_name] = "getlocations"
-libraries[getlocations_markers][destination] = "libraries"
--- project/profiles/capacity4more/modules/c4m/restful/c4m_restful/plugins/restful/node/events/1.0/C4mRestfulEventsResource.class.php
+++ project/profiles/capacity4more/modules/c4m/restful/c4m_restful/plugins/restful/node/events/1.0/C4mRestfulEventsResource.class.php
@@ -32,13 +32,6 @@ class C4mRestfulEventsResource extends C4mRestfulEntityBaseNode {
       'property' => 'c4m_datetime_end',
     );
 
-    $public_fields['location'] = array(
-      'property' => 'c4m_location',
-      'process_callbacks' => array(
-        array($this, 'processLocation'),
-      ),
-    );
-
     $public_fields['group'] = array(
       'property' => OG_AUDIENCE_FIELD,
       'resource' => array(
@@ -97,61 +90,13 @@ class C4mRestfulEventsResource extends C4mRestfulEntityBaseNode {
   }
 
   /**
-   * Location process callback.
-   *
-   * @param array $value
-   *   The array with the field value.
-   *
-   * @return array
-   *   Array with the necessary location values.
-   */
-  protected function processLocation(array $value) {
-    return array(
-      'street' => $value['street'],
-      'city' => $value['city'],
-      'postal_code' => $value['postal_code'],
-      'country_name' => $value['country_name'],
-      'country' => $value['country'],
-      'lat' => $value['latitude'],
-      'lng' => $value['longitude'],
-    );
-  }
-
-  /**
    * Overrides /ResfulEntityBase::createEntity().
    *
    * Add a new location when saving an event entity.
    */
   public function createEntity() {
     $entity = parent::createEntity();
-    $request = $this->getRequest();
     $entity = node_load($entity[0]['id']);
-
-    $locations = array(
-      0 => array(
-        'address' => $request['location']['street'] . ' ' . $request['location']['postal_code'] . ', ' . $request['location']['city'] . ', ' . $request['location']['country_name'],
-        'street' => $request['location']['street'],
-        'postal_code' => $request['location']['postal_code'],
-        'city' => $request['location']['city'],
-        'country_name' => $request['location']['country_name'],
-        'country' => $request['location']['country'],
-        'latitude' => $request['location']['lat'],
-        'longitude' => $request['location']['lng'],
-        'name' => $request['location']['location_name'],
-      ),
-    );
-
-    $criteria = array(
-      'field_name' => 'c4m_location',
-      'nid' => $entity->nid,
-      'vid' => $entity->vid,
-    );
-
-    $locations = getlocations_fields_save_locations($locations, $criteria, array(), 'insert');
-    if (count($locations) > 0) {
-      entity_metadata_wrapper('node', $entity)->c4m_location->set($locations[0]);
-      node_save($entity);
-    }
 
     $wrapper = entity_metadata_wrapper($this->entityType, $entity);
     return array($this->viewEntity($wrapper->getIdentifier()));
