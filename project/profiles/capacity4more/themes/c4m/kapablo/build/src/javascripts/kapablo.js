464f4887d1beadc15014a2c63a045fa58b319d3d
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-04-22 16:09:17 +0200

    Preserve the collapse/fullscreen state

--- project/profiles/capacity4more/themes/c4m/kapablo/build/src/javascripts/kapablo.js
+++ project/profiles/capacity4more/themes/c4m/kapablo/build/src/javascripts/kapablo.js
@@ -75,6 +75,13 @@
 
     Drupal.behaviors.sidebarCollapseExpand = {
         attach: function (context, settings) {
+            var url = location.href;
+            var fullscreen = _getParameter(url, 'fullscreen');
+
+          if (fullscreen === '1') {
+              collapseSidebar($('#collapse-sidebar'));
+          }
+
             $('#collapse-sidebar').on('click', function () {
                 var buttonClasses = $(this).attr("class");
                 // If the button has the "collapsed" class,
@@ -105,6 +112,13 @@
                 groupLeft.removeClass('col-sm-4').addClass('col-sm-1 sidebar-collapsed');
                 groupRight.removeClass('col-sm-8').addClass('col-sm-11');
                 sidebarContent.hide();
+
+                var nav_links = $('.og-menu-link.wiki .c4m-book-og-menu-link, #group-pages-navigation-left .field-name-c4m-content-wiki-page-navigation a');
+                var href;
+                $(nav_links).each(function () {
+                    href = _addParameter($(this).attr('href'), 'fullscreen', '1');
+                    $(this).attr('href', href);
+                });
             }
 
             /**
@@ -125,7 +139,75 @@
                 groupLeft.removeClass('col-sm-1 sidebar-collapsed').addClass('col-sm-4');
                 groupRight.removeClass('col-sm-11').addClass('col-sm-8');
                 sidebarContent.show();
+
+                var nav_links = $('.og-menu-link.wiki .c4m-book-og-menu-link, #group-pages-navigation-left .field-name-c4m-content-wiki-page-navigation a');
+                var href;
+                $(nav_links).each(function () {
+                    href = _removeURLParameter($(this).attr('href'), 'fullscreen');
+                    $(this).attr('href', href);
+                });
             }
+        }
+  };
+
+    Drupal.behaviors.wikiPagesSubNavigation = {
+        attach: function (context, settings) {
+            var url = location.href;
+            var expanded = _getParameter(url, 'expanded');
+
+          if (expanded === '1') {
+              var $wrapper = $('#group-pages-navigation-left');
+
+              // .wrapInner() does not retain bound events.
+              var wrapper = $wrapper.get(0);
+              // Don't animate multiple times.
+            if (!wrapper.animating) {
+                wrapper.animating = true;
+                var speed = $wrapper.hasClass('speed-fast') ? 300 : 1000;
+              if ($wrapper.hasClass('effect-none') && $wrapper.hasClass('speed-none')) {
+                  $('> .field-group-format-wrapper', wrapper).toggle();
+              }
+                else if ($wrapper.hasClass('effect-blind')) {
+                    $('> .field-group-format-wrapper', wrapper).toggle('blind', {}, speed);
+                }
+                else {
+                    $('> .field-group-format-wrapper', wrapper).toggle(speed);
+                }
+                wrapper.animating = false;
+                $wrapper.toggleClass('collapsed');
+
+                var nav_links = $('.og-menu-link.wiki .c4m-book-og-menu-link, #group-pages-navigation-left .field-name-c4m-content-wiki-page-navigation a');
+
+                var href;
+                $(nav_links).each(function () {
+                    href = _addParameter($(this).attr('href'), 'expanded', '1');
+                    $(this).attr('href', href);
+                });
+
+                  return false;
+            }
+          }
+
+            $('#group-pages-navigation-left .field-group-format-toggler a').on('click', function () {
+                var collapsedClass = $(this).parents('.group-pages').attr('class');
+
+              if (collapsedClass.indexOf("collapsed") >= 0) {
+                  var nav_links = $('.og-menu-link.wiki .c4m-book-og-menu-link, #group-pages-navigation-left .field-name-c4m-content-wiki-page-navigation a');
+                  var href;
+                  $(nav_links).each(function () {
+                      href = _removeURLParameter($(this).attr('href'), 'expanded');
+                      $(this).attr('href', href);
+                  });
+              }
+              else {
+                  var nav_links = $('.og-menu-link.wiki .c4m-book-og-menu-link', '#group-pages-navigation-left .field-name-c4m-content-wiki-page-navigation a');
+                  var href;
+                  $(nav_links).each(function () {
+                      href = _addParameter($(this).attr('href'), 'expanded', '1');
+                      $(this).attr('href', href);
+                  });
+              }
+            });
         }
   };
 
@@ -151,4 +233,119 @@
             }, 100);
         }
   }
-})(jQuery);
+
+    /**
+     * Remove a parameter from an URL string.
+     *
+     * @param url
+     *   Url or path to remove parameter from.
+     * @param parameter
+     *   Name of the parameter to remove.
+     *
+     * @returns {*}
+     *
+     * @private
+     */
+    function _removeURLParameter(url, parameter) {
+        // Prefer to use l.search if you have a location/link object.
+        var urlparts = url.split('?');
+    if (urlparts.length >= 2) {
+
+        var prefix = encodeURIComponent(parameter) + '=';
+        var pars = urlparts[1].split(/[&;]/g);
+
+        // Reverse iteration as may be destructive.
+      for (var i = pars.length; i-- > 0;) {
+          // Idiom for string.startsWith.
+        if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+            pars.splice(i, 1);
+        }
+      }
+
+      if (pars.length > 0) {
+          url = urlparts[0] + '?' + pars.join('&');
+      }
+      else {
+          url = urlparts[0];
+      }
+
+        return url;
+    }
+    else {
+        return url;
+    }
+    }
+
+    /**
+     * Add a parameter to an URL string.
+     *
+     * @param url
+     *   Url or path to add parameter to.
+     * @param parameterName
+     *   Name of the parameter to add.
+     * @param parameterValue
+     *   Value to give the parameter.
+     *
+     * @returns {string}
+     *
+     * @private
+     */
+    function _addParameter(url, parameterName, parameterValue) {
+        var replaceDuplicates = true;
+        var urlhash;
+        var cl;
+
+      if (url.indexOf('#') > 0) {
+          cl = url.indexOf('#');
+          urlhash = url.substring(url.indexOf('#'), url.length);
+      }
+      else {
+          urlhash = '';
+          cl = url.length;
+      }
+        var sourceUrl = url.substring(0, cl);
+
+        var urlParts = sourceUrl.split("?");
+        var newQueryString = "";
+
+      if (urlParts.length > 1) {
+          var parameters = urlParts[1].split("&");
+        for (var i = 0; (i < parameters.length); i++) {
+            var parameterParts = parameters[i].split("=");
+          if (!(replaceDuplicates && parameterParts[0] === parameterName)) {
+            if (newQueryString === "") {
+                newQueryString = "?";
+            }
+            else {
+              newQueryString += "&";
+            }
+              newQueryString += parameterParts[0] + "=" + (parameterParts[1] ? parameterParts[1] : '');
+          }
+        }
+      }
+      if (newQueryString === "") {
+          newQueryString = "?";
+      }
+
+      if (newQueryString !== "" && newQueryString !== '?') {
+          newQueryString += "&";
+      }
+        newQueryString += parameterName + "=" + (parameterValue ? parameterValue : '');
+        return urlParts[0] + newQueryString + urlhash;
+    };
+
+    function _getParameter(url, parameter) {
+        var urlParts = url.split("?");
+
+      if (urlParts.length > 1) {
+          var parameters = urlParts[1].split("&");
+        for (var i = 0; (i < parameters.length); i++) {
+            var parameterParts = parameters[i].split("=");
+          if (parameterParts[0] === parameter) {
+              return parameterParts[1];
+          }
+        }
+      }
+    };
+})
+(jQuery);
