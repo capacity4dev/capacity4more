4147457a404fd4045048314576fae4e2b6f3fb97
Jan Lemmens <jan.lemmens@amplexor.com>
2016-05-18 13:33:49 +0200

    CFM-1404: updated tests + PHPCS + site_403 variable.

--- project/profiles/capacity4more/behat/features/bootstrap/FeatureContext/Node.php
+++ project/profiles/capacity4more/behat/features/bootstrap/FeatureContext/Node.php
@@ -57,7 +57,7 @@ trait Node {
 
     return array(
       new Step\When('I go to "node/add/'.$type.'"'),
-      new Step\Then('I should see "Please log in to continue"'),
+      new Step\Then('I should see "Access denied"'),
     );
   }
 
@@ -68,7 +68,7 @@ trait Node {
 
     return array(
       new Step\When('I go to "node/add/'.$type.'"'),
-      new Step\Then('I should not see "Please log in to continue"'),
+      new Step\Then('I should not see "Access denied"'),
     );
   }
 
