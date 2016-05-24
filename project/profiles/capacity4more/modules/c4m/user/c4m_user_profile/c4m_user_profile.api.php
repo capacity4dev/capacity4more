97b4e688a991768bbf4f9faaacd86d79324cda84
Mircea Fernea <mircea.fernea@amplexor.com>
2016-05-24 14:07:11 +0300

    CFM-435: Create LDAP validator.

--- project/profiles/capacity4more/modules/c4m/user/c4m_user_profile/c4m_user_profile.api.php
+++ project/profiles/capacity4more/modules/c4m/user/c4m_user_profile/c4m_user_profile.api.php
@@ -16,25 +16,8 @@
 function hook_c4m_user_profile_validate_email($mail, $form_state, $form) {
   return array(
     array(
-      'message' => t('Error message'),
-      '#weight' => 9,
-    ),
-  );
-}
-
-/**
- * Validates an email address only on AJAX calls.
- *
- * @param string $mail
- *   Email address.
- *
- * @return array
- *   An array of messages array. Error have the #weight key.
- */
-function hook_c4m_user_profile_validate_email_ajax($mail, $form_state, $form) {
-  return array(
-    array(
       'message' => t('Message'),
+      'type' => C4M_USER_PROFILE_VALIDATION_MESSAGE_TYPE_ERROR | C4M_USER_PROFILE_VALIDATION_MESSAGE_TYPE_AJAX,
       '#weight' => 9,
     ),
   );
