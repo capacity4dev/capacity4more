e95fc667438f9e061576dfd5fff7f6a4b40ebd65
Mircea Fernea <mircea.fernea@amplexor.com>
2016-05-25 15:38:23 +0300

    CFM-437, CFM-438: Alter form only on user registration process.

--- project/profiles/capacity4more/modules/c4m/user/c4m_user_profile/c4m_user_profile.api.php
+++ project/profiles/capacity4more/modules/c4m/user/c4m_user_profile/c4m_user_profile.api.php
@@ -7,18 +7,19 @@
 /**
  * Validates an email address.
  *
- * @param string $mail
- *   Email address.
- *
- * @return array
- *   An array of error messages array. Error have the #weight key.
+ * @param array $data
+ *   Contains context information like:
+ *   - email;
+ *   - minimum_error_level;
+ *   - form;
+ *   - form_state;
+ *   - alter_form;
+ *   - validation_messages.
  */
-function hook_c4m_user_profile_validate_email($mail, $form_state, $form) {
-  return array(
-    array(
-      'message' => t('Message'),
-      'type' => C4M_USER_PROFILE_VALIDATION_MESSAGE_TYPE_ERROR,
-      '#weight' => 9,
-    ),
+function hook_c4m_user_profile_validate_email_alter(&$data) {
+  $data['validation_messages'][] = array(
+    'message' => t('The e-mail address %mail is not valid.', array('%mail' => $data['mail'])),
+    'type' => C4M_USER_PROFILE_VALIDATION_MESSAGE_TYPE_ERROR,
+    '#weight' => -6,
   );
 }
