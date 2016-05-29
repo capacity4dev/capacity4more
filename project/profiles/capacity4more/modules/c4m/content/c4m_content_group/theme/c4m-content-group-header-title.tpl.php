ede7bf69867b46aa529d3afbd7dd71fd6d915787
anatoly vaitsman <anatoly@gizra.com>
2016-05-29 16:48:48 +0300

    WIP: Add information fields to group title header.

---
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_group/theme/c4m-content-group-header-title.tpl.php
@@ -0,0 +1,29 @@
+<div class="row" id="content-group-header-title">
+  <div class="col-sm-12 col-md-8">
+    <h1><?php print $title;?></h1>
+  </div>
+  <div class="col-sm-12 col-md-4" id="content-group-header-title-right-div">
+    <div class="row">
+      <div class="col-xs-offset-7 col-xs-5">
+        <div class="top-buffer indication indication-type">
+          group
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-2">
+        <span class="top-buffer group-icon group-<?php print $group_access; ?> node-icon as-group-<?php print $group_access; ?>"></span>
+      </div>
+      <div class="col-xs-5">
+        <div class="top-buffer indication indication-access <?php print $group_access; ?>">
+          <?php print $group_access; ?>
+        </div>
+      </div>
+      <div class="col-xs-5">
+        <div class="top-buffer indication indication-status">
+          <?php print $group_status; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
