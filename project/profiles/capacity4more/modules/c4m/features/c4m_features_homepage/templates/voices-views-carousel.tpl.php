bc206b7df0488bd92a446baf07658705efc2302d
Nir Galon <nirgn975@gmail.com>
2016-05-29 15:57:54 +0300

    Carousel works.

--- project/profiles/capacity4more/modules/c4m/features/c4m_features_homepage/templates/voices-views-carousel.tpl.php
+++ project/profiles/capacity4more/modules/c4m/features/c4m_features_homepage/templates/voices-views-carousel.tpl.php
@@ -9,26 +9,14 @@
   <span class="block-title"><?php print l(t('VOICES & VIEWS'), 'articles'); ?></span>
   <div class="col-md-12">
     <div class="owl-carousel">
-      <div>
-        <h2>Your Content 1</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-      </div>
-      <div>
-        <h2>Your Content 2</h2>
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
-      </div>
-      <div>
-        <h2>Your Content 3</h2>
-        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
-      </div>
-      <div>
-        <h2>Your Content 4</h2>
-        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-      </div>
-      <div>
-        <h2>Your Content 5</h2>
-        <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form</p>
-      </div>
+      <?php foreach ($carousels as $carousel): ?>
+        <div class="item">
+          <img class="" src="<?print $carousel['image']; ?>">
+          <h2><?php print $carousel['title']; ?></h2>
+          <p><?php print $carousel['text']; ?></p>
+          <p><?php print $carousel['link']; ?></p>
+        </div>
+      <?php endforeach; ?>
     </div>
   </div>
 </div>
