ee39c3c772c71b4c5b4d589aa76a670c167b183e
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-05-31 17:59:17 +0200

    PHPCBFD fixed

--- project/profiles/capacity4more/themes/c4m/kapablo/build/src/javascripts/classie.js
+++ project/profiles/capacity4more/themes/c4m/kapablo/build/src/javascripts/classie.js
@@ -1,8 +1,10 @@
-/*!
+/**
+ * @file
+ * @file!
  * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
+ * from bonzo https://github.com/ded/bonzo.
  *
- * classie.has( elem, 'my-class' ) -> true/false
+ * Classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
  * classie.remove( elem, 'my-unwanted-class' )
  * classie.toggle( elem, 'my-class' )
@@ -11,72 +13,72 @@
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
 
-( function( window ) {
+(function (window) {
 
-'use strict';
+    'use strict';
 
-// class helper functions from bonzo https://github.com/ded/bonzo
+  // Class helper functions from bonzo https://github.com/ded/bonzo
+  function classReg(className) {
+      return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+  }
 
-function classReg( className ) {
-  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-}
+  // classList support for class management
+  // altho to be fair, the api sucks because it won't accept multiple classes at once.
+    var hasClass, addClass, removeClass;
 
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
-var hasClass, addClass, removeClass;
+  if ('classList' in document.documentElement) {
+      hasClass = function (elem, c) {
+          return elem.classList.contains(c);
+      };
+      addClass = function (elem, c) {
+          console.log("add class (classList exist: " + elem);
+          elem.classList.add(c);
+      };
+      removeClass = function (elem, c) {
+          elem.classList.remove(c);
+      };
+  }
+  else {
+      hasClass = function (elem, c) {
+          return classReg(c).test(elem.className);
+      };
+      addClass = function (elem, c) {
+          console.log("add class: " + c);
+        if (!hasClass(elem, c)) {
+            elem.className = elem.className + ' ' + c;
+        }
+      };
+      removeClass = function (elem, c) {
+          elem.className = elem.className.replace(classReg(c), ' ');
+      };
+  }
 
-if ( 'classList' in document.documentElement ) {
-  hasClass = function( elem, c ) {
-    return elem.classList.contains( c );
-  };
-  addClass = function( elem, c ) {
-    console.log("add class (classList exist: "+elem);
-    elem.classList.add( c );
-  };
-  removeClass = function( elem, c ) {
-    elem.classList.remove( c );
-  };
-}
-else {
-  hasClass = function( elem, c ) {
-    return classReg( c ).test( elem.className );
-  };
-  addClass = function( elem, c ) {
-    console.log("add class: "+c);
-    if ( !hasClass( elem, c ) ) {
-      elem.className = elem.className + ' ' + c;
-    }
-  };
-  removeClass = function( elem, c ) {
-    elem.className = elem.className.replace( classReg( c ), ' ' );
-  };
-}
+  function toggleClass(elem, c) {
+      var fn = hasClass(elem, c) ? removeClass : addClass;
+      fn(elem, c);
+  }
 
-function toggleClass( elem, c ) {
-  var fn = hasClass( elem, c ) ? removeClass : addClass;
-  fn( elem, c );
-}
+    var classie = {
+        // Full names.
+        hasClass: hasClass,
+        addClass: addClass,
+        removeClass: removeClass,
+        toggleClass: toggleClass,
+        // Short names.
+        has: hasClass,
+        add: addClass,
+        remove: removeClass,
+        toggle: toggleClass
+    };
 
-var classie = {
-  // full names
-  hasClass: hasClass,
-  addClass: addClass,
-  removeClass: removeClass,
-  toggleClass: toggleClass,
-  // short names
-  has: hasClass,
-  add: addClass,
-  remove: removeClass,
-  toggle: toggleClass
-};
+  // Transport.
+  if (typeof define === 'function' && define.amd) {
+      // AMD.
+      define(classie);
+  }
+  else {
+      // Browser global.
+      window.classie = classie;
+  }
 
-// transport
-if ( typeof define === 'function' && define.amd ) {
-  // AMD
-  define( classie );
-} else {
-  // browser global
-  window.classie = classie;
-}
-
-})( window );
+})(window);
