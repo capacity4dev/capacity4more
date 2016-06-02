1ca2c924a586d2edd7f13ea20257b96a73a58419
Nir Galon <nirgn975@gmail.com>
2016-06-02 11:54:45 +0300

    Style the carousel.

--- project/profiles/capacity4more/modules/c4m/features/c4m_features_homepage/templates/voices-views-carousel.tpl.php
+++ project/profiles/capacity4more/modules/c4m/features/c4m_features_homepage/templates/voices-views-carousel.tpl.php
@@ -6,15 +6,14 @@
 ?>
 
 <div class="row carousel">
-  <span class="block-title"><?php print l(t('VOICES & VIEWS'), 'articles'); ?></span>
+  <span class="block-title"><?php print l(t('Latest Articles'), 'articles'); ?></span>
   <div class="col-md-12">
     <div class="owl-carousel">
       <?php foreach ($carousels as $carousel): ?>
         <div class="item">
           <?php print $carousel['image']; ?>
           <h2><?php print $carousel['title']; ?></h2>
-          <p class="intro-text"><?php print $carousel['text']; ?></p>
-          <p><?php print $carousel['link']; ?></p>
+          <p class="intro-text"><?php print $carousel['text'] . ' | ' . $carousel['link']; ?></p>
         </div>
       <?php endforeach; ?>
     </div>
