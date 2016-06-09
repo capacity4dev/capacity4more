7246bf9922e85812ade9770f94017ec32061ffe1
Jan Lemmens <jan.lemmens@amplexor.com>
2016-06-09 10:41:32 +0200

    CFM-1271: code cleanup.

--- project/profiles/capacity4more/modules/c4m/features_og/c4m_features_og_manage/plugins/content_types/project_details/project-details.tpl.php
+++ project/profiles/capacity4more/modules/c4m/features_og/c4m_features_og_manage/plugins/content_types/project_details/project-details.tpl.php
@@ -16,8 +16,8 @@
         <div class="col-md-9"><?php print $details['owner']; ?></div>
       </div>
       <div class="row">
-        <div class="col-md-3"><?php print t('Members') ?> (<a href="<?php print $details['admins_edit']; ?>"><?php print t('Edit') ?></a>)</div>
-        <div class="col-md-9"><?php print $details['admins']; ?></div>
+        <div class="col-md-3"><?php print t('Members') ?> (<a href="<?php print $details['members_edit']; ?>"><?php print t('Edit') ?></a>)</div>
+        <div class="col-md-9"><?php print $details['members']; ?></div>
       </div>
     </div>
     <div class="project-details">
