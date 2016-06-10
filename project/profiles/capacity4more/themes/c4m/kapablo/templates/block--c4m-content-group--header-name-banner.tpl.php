e2b047010c17255cc9361b46d3bb4c04ac096c4e
Stijn Hallet <stijn.hallet@amplexor.com>
2016-06-10 16:38:09 +0200

    add file description

--- project/profiles/capacity4more/themes/c4m/kapablo/templates/block--c4m-content-group--header-name-banner.tpl.php
+++ project/profiles/capacity4more/themes/c4m/kapablo/templates/block--c4m-content-group--header-name-banner.tpl.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * @file
+ * Custom group header
+ */
+?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> group-header"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
     <?php print $content ?>
