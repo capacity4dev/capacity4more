c36a3309d6ea7297a9cf1085d9e66f123637038d
Stijn Hallet <stijn.hallet@amplexor.com>
2016-06-10 16:11:36 +0200

    remove HTML comments

--- project/profiles/capacity4more/themes/c4m/kapablo/templates/block--c4m-content-group--header-name-banner.tpl.php
+++ project/profiles/capacity4more/themes/c4m/kapablo/templates/block--c4m-content-group--header-name-banner.tpl.php
@@ -1,7 +1,5 @@
-<!-- title & image -->
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> group-header"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
     <?php print $content ?>
   </div>
 </div>
-<!-- end title & image -->
