633dbbb386cc4d9395042264f6415b68c8ec67b2
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-06-15 17:20:08 +0200

    Group header updated

--- project/profiles/capacity4more/modules/c4m/content/c4m_content_group/theme/c4m-content-group-header-title.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_group/theme/c4m-content-group-header-title.tpl.php
@@ -7,32 +7,28 @@
  * Type, Status and Access at content group header title.
  */
 ?>
-<div class="row" id="content-group-header-title">
-  <div class="col-sm-12 col-md-8">
-    <?php print $title;?>
-  </div>
-  <div class="col-sm-12 col-md-4" id="content-group-header-title-right-div">
-    <div class="row">
-      <div class="col-xs-offset-7 col-xs-5">
-        <div class="top-buffer indication label label-default">
-          group
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-2">
-        <span class="top-buffer group-icon group-<?php print $group_access; ?> node-icon as-group-<?php print $group_access; ?>"></span>
-      </div>
-      <div class="col-xs-5">
-        <div class="top-buffer indication label label-access <?php print $group_access; ?>">
-          <?php print $group_access; ?>
-        </div>
-      </div>
-      <div class="col-xs-5">
-        <div class="top-buffer indication label label-default">
-          <?php print $group_status; ?>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+
+<?php if($title): ?>
+  <h1>
+    <?php print $title; ?>
+  </h1>
+<?php endif; ?>
+
+<?php if ($group_type): ?>
+  <span class="top-buffer indication label label-default">
+    <?php print $group_type; ?>
+  </span>
+<?php endif; ?>
+
+<?php if ($group_access): ?>
+  <i class="top-buffer group-icon group-<?php print $group_access; ?> node-icon as-group-<?php print $group_access; ?>"></i>
+  <span class="top-buffer indication label label-access <?php print $group_access; ?>">
+    <?php print $group_access; ?>
+  </span>
+<?php endif; ?>
+
+<?php if ($group_status): ?>
+  <span class="top-buffer indication label label-default">
+    <?php print $group_status; ?>
+  </span>
+<?php endif; ?>
