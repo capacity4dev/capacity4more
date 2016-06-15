4a57d0e5ef6be3e7b648ba0dc64e2870d37f22b3
Stijn Hallet <stijn.hallet@amplexor.com>
2016-06-15 12:29:42 +0200

    refactor the group header

--- project/profiles/capacity4more/themes/c4m/kapablo/templates/block--menu--c4m-og-menu.tpl.php
+++ project/profiles/capacity4more/themes/c4m/kapablo/templates/block--menu--c4m-og-menu.tpl.php
@@ -5,18 +5,8 @@
  */
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> group-navigation"<?php print $attributes; ?>>
-
-  <span class="label">GROUP</span>
-  <span class="label">DRAFT</span>
-
-  <button type="button" class="navbar-toggle collapsed header-actions--navigation" data-toggle="collapse" data-target="#c4m-og-menu">
-    <span class="sr-only">Toggle navigation</span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-  </button>
-
   <nav role="navigation" class="collapse navbar-collapse" id="c4m-og-menu">
         <?php print $content ?>
   </nav>
 </div>
+<hr class="groupNavigation-divider" />
