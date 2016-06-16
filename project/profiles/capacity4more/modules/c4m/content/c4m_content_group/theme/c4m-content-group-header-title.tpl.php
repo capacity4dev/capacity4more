d4202bba728dbc2074b51506249f3ce7bd2127d2
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-06-16 09:17:06 +0200

    Add restricted groups info to the group title header.

--- project/profiles/capacity4more/modules/c4m/content/c4m_content_group/theme/c4m-content-group-header-title.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_group/theme/c4m-content-group-header-title.tpl.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-<?php if($title): ?>
+<?php if ($title): ?>
   <h1>
     <?php print $title; ?>
   </h1>
@@ -25,6 +25,38 @@
   <span class="top-buffer indication label label-access <?php print $group_access; ?>">
     <?php print $group_access; ?>
   </span>
+
+  <?php if ($organisations): ?>
+    <?php if ($organisation_icons): ?>
+      <div class="restricted-organisation-icons">
+        <?php foreach ($organisation_icons as $organisation_icon) : ?>
+          <span class="restricted-organisation-icon">
+            <?php print $organisation_icon; ?>
+          </span>
+        <?php endforeach; ?>
+
+        <?php if ($organisation_ellipsis): ?>
+          <span class="restricted-organisation-icon ellipsis">
+            ...
+          </span>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+
+    <div class="restricted-extra-wrapper">
+      <div class="restricted-organisations">
+        <?php foreach ($organisations as $organisation) : ?>
+          <?php print $organisation; ?>
+        <?php endforeach; ?>
+      </div>
+      <div class="restricted-emails">
+        <?php foreach ($emails as $email) : ?>
+          <?php print $email; ?>
+        <?php endforeach; ?>
+      </div>
+    </div>
+
+  <?php endif; ?>
 <?php endif; ?>
 
 <?php if ($group_status): ?>
