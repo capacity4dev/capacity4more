f28ce45b88b229548c330547678c924a707e321b
Jochen Verdeyen <jochen.verdeyen@amplexor.com>
2016-06-21 19:54:23 +0200

    CFM-1441: Search page title

--- project/profiles/capacity4more/themes/c4m/kapablo/templates/block--menu--c4m-og-menu.tpl.php
+++ project/profiles/capacity4more/themes/c4m/kapablo/templates/block--menu--c4m-og-menu.tpl.php
@@ -6,7 +6,7 @@
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> group-navigation"<?php print $attributes; ?>>
   <nav role="navigation" class="collapse navbar-collapse" id="c4m-og-menu">
-        <?php print $content ?>
+    <?php print $content; ?>
   </nav>
 </div>
 <hr class="groupNavigation-divider" />
