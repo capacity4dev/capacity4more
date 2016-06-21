6f82e2d60b5f39fd5bb9739700d8f30d58fb6d3b
Jochen Verdeyen <jochen.verdeyen@amplexor.com>
2016-06-21 09:14:32 +0200

    CFM-1438: Add search menu to the responsive navigation in groups

--- project/profiles/capacity4more/themes/c4m/kapablo/templates/block--menu--c4m-og-menu.tpl.php
+++ project/profiles/capacity4more/themes/c4m/kapablo/templates/block--menu--c4m-og-menu.tpl.php
@@ -6,7 +6,10 @@
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> group-navigation"<?php print $attributes; ?>>
   <nav role="navigation" class="collapse navbar-collapse" id="c4m-og-menu">
-        <?php print $content ?>
+    <?php if (isset($search)): ?>
+      <?php print $search; ?>
+    <?php endif; ?>
+    <?php print $content; ?>
   </nav>
 </div>
 <hr class="groupNavigation-divider" />
