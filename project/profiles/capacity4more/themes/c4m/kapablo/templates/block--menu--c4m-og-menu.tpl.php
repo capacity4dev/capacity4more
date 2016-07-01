f3a930bd038ba02ed7c4782791abe344c1664253
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-07-01 12:21:53 +0200

    Add search to the group navigation dropout

--- project/profiles/capacity4more/themes/c4m/kapablo/templates/block--menu--c4m-og-menu.tpl.php
+++ project/profiles/capacity4more/themes/c4m/kapablo/templates/block--menu--c4m-og-menu.tpl.php
@@ -4,9 +4,15 @@
  * Custom menu theme implementation to display a block.
  */
 ?>
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> group-navigation"<?php print $attributes; ?>>
+<div id="<?php print $block_html_id; ?>"
+     class="<?php print $classes; ?> group-navigation"<?php print $attributes; ?>>
   <nav role="navigation" class="collapse navbar-collapse" id="c4m-og-menu">
+    <?php if ($search_form): ?>
+      <!-- SEARCH-->
+      <?php print render($search_form); ?>
+    <?php endif; ?>
+
     <?php print $content; ?>
   </nav>
 </div>
-<hr class="groupNavigation-divider" />
+<hr class="groupNavigation-divider"/>
