725355254955fe048a50ea7a83a7e14625d65e39
anatoly vaitsman <anatoly@gizra.com>
2016-07-03 19:02:29 +0300

    Code review fixes.

--- project/profiles/capacity4more/modules/c4m/content/c4m_content_project/theme/c4m-content-project-header-title.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_project/theme/c4m-content-project-header-title.tpl.php
@@ -12,14 +12,14 @@
       <?php print $title;?>
     </h1>
   </div>
-  <div class="col-sm-12 col-md-4" id="content-project-header-title-right-div">
+  <div class="col-sm-12 col-md-4">
     <div class="row">
       <div class="col-xs-2">
-        <span class="fa fa-flag<?php print $flag; ?> indication top-buffer project-icon <?php print $prj_stage?>">
+        <span class="fa <?php print $flag_icon; ?> indication top-buffer project-icon <?php print $project_stage?>">
         </span>
       </div>
       <div class="col-xs-5">
-        <div class="top-buffer indication label label-stage <?php print $prj_stage?>">
+        <div class="top-buffer indication label label-stage <?php print $project_stage?>">
           <?php print $stage; ?>
         </div>
       </div>
