1a0de592031c04e80957a5dda21494592795fa43
anatoly vaitsman <anatoly@gizra.com>
2016-07-03 13:54:12 +0300

    CSS styling

--- project/profiles/capacity4more/modules/c4m/content/c4m_content_project/theme/c4m-content-project-header-title.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_project/theme/c4m-content-project-header-title.tpl.php
@@ -8,16 +8,18 @@
 ?>
 <div class="row" id="content-project-header-title">
   <div class="col-sm-12 col-md-8">
-    <?php print $title;?>
+    <h1>
+      <?php print $title;?>
+    </h1>
   </div>
   <div class="col-sm-12 col-md-4" id="content-project-header-title-right-div">
     <div class="row">
       <div class="col-xs-2">
-        <span class="fa fa-flag<?php print $flag; ?> top-buffer project-<?php print $stage?>">
+        <span class="fa fa-flag<?php print $flag; ?> indication top-buffer project-icon <?php print $prj_stage?>">
         </span>
       </div>
       <div class="col-xs-5">
-        <div class="top-buffer indication label label-default">
+        <div class="top-buffer indication label label-stage <?php print $prj_stage?>">
           <?php print $stage; ?>
         </div>
       </div>
