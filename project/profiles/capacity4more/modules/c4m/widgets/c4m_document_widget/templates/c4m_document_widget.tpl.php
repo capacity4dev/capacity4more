3c504997899d09d4fc98537bc02c74724daf8411
Guy Tepper <guytepper@gmail.com>
2016-07-04 16:45:13 +0300

    Removed line break from document upload placeholder

--- project/profiles/capacity4more/modules/c4m/widgets/c4m_document_widget/templates/c4m_document_widget.tpl.php
+++ project/profiles/capacity4more/modules/c4m/widgets/c4m_document_widget/templates/c4m_document_widget.tpl.php
@@ -16,7 +16,6 @@
       <?php print t('Drop file here to upload or'); ?>
       <a href="javascript://" ng-click="browseFiles('<?php print $field_name; ?>')"> <?php print t('Browse'); ?> </a>
       <input type="file" name="document-file" id="<?php print $field_name; ?>" class="document_file" ng-file-select="onFileSelect($files, '<?php print $field_name; ?>')">
-      <br/>
       <?php print t('or'); ?>
       <a href="<?php print url('overlay/documents', array('absolute' => TRUE, 'purl' => array('disabled' => FALSE))); ?>" ng-click="setFieldName('<?php print $field_name; ?>')" id="link-<?php print $field_name; ?>" ng-class="{'active-library-link':fieldName === '<?php print $field_name; ?>'}">
         <?php print t('Select a Document from the library') ?>
