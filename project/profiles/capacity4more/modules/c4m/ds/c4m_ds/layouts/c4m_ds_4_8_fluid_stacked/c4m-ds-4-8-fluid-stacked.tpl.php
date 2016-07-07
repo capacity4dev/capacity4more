1ebb6cc20733ced56d4fd0a43e71a9a65f41523e
Guy Tepper <guytepper@gmail.com>
2016-07-07 18:20:24 +0300

    Reverted line indentions

--- project/profiles/capacity4more/modules/c4m/ds/c4m_ds/layouts/c4m_ds_4_8_fluid_stacked/c4m-ds-4-8-fluid-stacked.tpl.php
+++ project/profiles/capacity4more/modules/c4m/ds/c4m_ds/layouts/c4m_ds_4_8_fluid_stacked/c4m-ds-4-8-fluid-stacked.tpl.php
@@ -13,16 +13,16 @@
 
 <div class="content clearfix">
   <<?php print $header_wrapper; ?> class="group-header col-xs-12 <?php print $header_classes; ?>">
-  <?php print $header; ?>
+    <?php print $header; ?>
   </<?php print $header_wrapper; ?>>
   <<?php print $left_wrapper; ?> class="group-left col-xs-4 <?php print $left_classes; ?>">
-  <?php print $left; ?>
+   <?php print $left; ?>
   </<?php print $left_wrapper; ?>>
   <<?php print $right_wrapper; ?> class="group-right col-xs-8 message-summary<?php print $right_classes; ?>">
-  <?php print $right; ?>
+    <?php print $right; ?>
   </<?php print $right_wrapper; ?>>
   <<?php print $footer_wrapper; ?> class="group-footer col-sm-12 <?php print $footer_classes; ?>">
-  <?php print $footer; ?>
+    <?php print $footer; ?>
   </<?php print $footer_wrapper; ?>>
 </div>
 
