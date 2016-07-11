967f4f1c6f7bef578270883644bb39d834778188
Or David <or@gizra.com>
2016-07-11 20:30:11 +0300

    Create the callbacks.

---
+++ project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_unified_workflow_register.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Template to render the title of the registration form under unified workflow.
+ */
+?>
+<div class="c4m-unified-workflow-title">
+  <h1 class="title">
+    To join the <strong><?php print $group_title; ?></strong> group you need to register with capacity4more by filling in your details below.
+  </h1>
+  <h3 class="login">
+    Already a member? <?php print $login_link; ?>
+  </h3>
+</div>
