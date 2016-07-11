8d06d6374f8d4d30c64901c6a0c2141879560793
Or David <or@gizra.com>
2016-07-11 17:49:23 +0300

    WIP

--- project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_group_visitor_cta.tpl.php
+++ project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_group_visitor_cta.tpl.php
@@ -6,9 +6,9 @@
  */
 ?>
 <div class="c4m-group-visitor-cta">
-  <button class="btn">
+  <a class="btn" href="<?php print $button_link; ?>">
     <i class="fa <?php print $button_icon; ?>"></i>
     <?php print $button_label; ?>
-  </button>
+  </a>
   Please <?php print $login_url; ?> first if you have a C4D account
 </div>
