91253a8220d0fe3949a03af2e915bfd0c3bcd34f
Or David <or@gizra.com>
2016-07-17 08:10:44 +0300

    Fix the PR.

--- project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_unified_workflow_register.tpl.php
+++ project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_unified_workflow_register.tpl.php
@@ -6,10 +6,10 @@
  */
 ?>
 <div class="c4m-unified-workflow-title">
-  <h1 class="title">
+  <h3 class="title">
     To join the <strong><?php print $group_title; ?></strong> group you need to register with capacity4more by filling in your details below.
-  </h1>
-  <h3 class="login">
-    Already a member? <?php print $login_link; ?>
   </h3>
+  <h4 class="login">
+    Already a member? <?php print $login_link; ?>
+  </h4>
 </div>
