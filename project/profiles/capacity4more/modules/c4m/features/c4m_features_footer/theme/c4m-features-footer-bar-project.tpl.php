d88cee51327b0b9ba5cc47e13e9589058a8ada1b
anatoly vaitsman <anatoly@gizra.com>
2016-07-20 16:29:56 +0300

    Move project footer links rendering from footer template into theme function.

--- project/profiles/capacity4more/modules/c4m/features/c4m_features_footer/theme/c4m-features-footer-bar-project.tpl.php
+++ project/profiles/capacity4more/modules/c4m/features/c4m_features_footer/theme/c4m-features-footer-bar-project.tpl.php
@@ -9,21 +9,14 @@
 <div>
   <ul class="c4m-footer-bar">
     <li class="first">
-      <a class="logo" href="<?php print $front_page; ?>"
-         title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
-      </a>
+      <?php print $logo_link ?>
     </li>
     <li class="c4m-footer-bar-project">
       <div class="text-uppercase">
         <?php print t('Explore more projects on') ?>
-        <a href="<?php print $project_page; ?>" class="text-orange">
-          <?php print t('Capacity4dev &gt') ?>
-        </a>
+        <?php print $project_link ?>
         <br/>
-        <a href="https://ec.europa.eu/europeaid/home_en">
-          <?php print t('International cooperation and development &gt') ?>
-        </a>
+        <?php print $europaid_link ?>
       </div>
     </li>
   </ul>
