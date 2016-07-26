23c31988e3cd436879121c9de1723578b82f2c4e
Nir Galon <nirgn975@gmail.com>
2016-07-26 08:43:24 +0300

    Revert preprocess changes.

--- project/profiles/capacity4more/modules/c4m/content/c4m_content_photo/theme/node-gallery-api-item-navigator.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_photo/theme/node-gallery-api-item-navigator.tpl.php
@@ -23,7 +23,14 @@
   <div class="navigator--center">
     <?php if ($gallery_link): ?>
       <div class="back-link">
-        <?php print $back_to_galley_link; ?>
+        <?php
+          global $base_path;
+          $options = array(
+            'absolute' => TRUE,
+          );
+          print l("Back to photo album", str_replace($base_path, '', $gallery_link), $options);
+        ?>
+<!--        --><?php //print $back_to_galley_link; ?>
       </div>
     <?php endif; ?>
     <?php if ($navigator['total'] > 0): ?>
