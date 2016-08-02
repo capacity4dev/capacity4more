22bf761cf6acd95afb1b14388206a533f441450c
anatoly vaitsman <anatoly@gizra.com>
2016-08-02 11:48:22 +0300

    WIP #2

---
+++ project/profiles/capacity4more/modules/c4m/ds/c4m_ds/templates/c4m_ds_group_notification_toggle.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Template to output the views count in the entity stats.
+ *
+ * Available variables:
+ * - $count : the views count.
+ */
+?>
+
+<span>
+  <?php print $message_link; ?>
+</span>
