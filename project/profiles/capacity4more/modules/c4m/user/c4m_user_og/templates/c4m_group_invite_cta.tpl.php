84df4318068e20742e565d9dbc0cf049728c4f44
yBaras <yoav@gizra.com>
2016-08-02 09:30:48 +0300

    Add invite_users custom permissions.

---
+++ project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_group_invite_cta.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Template to render the CTA block for a visitor.
+ */
+?>
+<div class="c4m-group-invite-cta">
+  <a class="btn" href="<?php print $url; ?>">
+    <i class="fa <?php print $button_icon; ?>"></i>
+    <?php print $button_label; ?>
+  </a>
+</div>
