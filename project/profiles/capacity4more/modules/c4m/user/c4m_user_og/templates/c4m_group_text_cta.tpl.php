8cee0bec3cf5e389b26ad25461a5b46ae9e366c9
David Hernández <davidhernandezruiz@gmail.com>
2016-08-03 16:47:41 +0200

    Updates the CTA to better follow up the design.

--- project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_group_text_cta.tpl.php
+++ project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_group_text_cta.tpl.php
@@ -5,7 +5,7 @@
  * Template to render the CTA block for a visitor.
  */
 ?>
-<div class="c4m-group-text-cta">
+<div class="c4m-group-text-cta small">
     <i class="fa <?php print $icon; ?>"></i>
     <?php print $text; ?>
   </a>
