b19deb02c911d10a7ae14dda6d0cb25ead0c3baf
David Hernández <davidhernandezruiz@gmail.com>
2016-08-03 12:48:06 +0200

    Adds new theme function for CTAs that are not buttons.

---
+++ project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_group_text_cta.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Template to render the CTA block for a visitor.
+ */
+?>
+<div class="c4m-group-text-cta">
+    <i class="fa <?php print $icon; ?>"></i>
+    <?php print $text; ?>
+  </a>
+</div>
