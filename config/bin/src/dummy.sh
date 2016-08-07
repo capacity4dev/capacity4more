c75e0a57bbde517ec613bf6686fe4cccf70086c0
anatoly vaitsman <anatoly@gizra.com>
2016-08-07 18:34:39 +0300

    Don't trigger notifications when activated from CLI (in case of dummy content migrations, for example).

--- config/bin/src/dummy.sh
+++ config/bin/src/dummy.sh
@@ -22,7 +22,5 @@ function dummy_content_modules {
 # 'dummy_content_migration' is set to 1 during the process, 0 when done.
 ##
 function dummy_content_migrate {
-  drupal_drush vset --exact dummy_content_migration 1
   drupal_drush --uri="$SITE_URL" mi --force --update --group=c4m_demo_content
-  drupal_drush vset --exact dummy_content_migration 0
 }
