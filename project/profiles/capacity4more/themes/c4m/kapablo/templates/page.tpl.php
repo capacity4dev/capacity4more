809fe2c7f16d326e4d1330b8ee8212d63efa33d0
Nir Galon <nirgn975@gmail.com>
2016-08-08 16:47:17 +0300

    Remove the title from the template page.

--- project/profiles/capacity4more/themes/c4m/kapablo/templates/page.tpl.php
+++ project/profiles/capacity4more/themes/c4m/kapablo/templates/page.tpl.php
@@ -249,11 +249,13 @@
             ); ?></div>
         <?php endif; ?>
         <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if (!empty($title)): ?>
-          <h1 class="page-header"><?php print $title; ?></h1>
+        <?php if (!isset($hide_title)): ?>
+          <?php print render($title_prefix); ?>
+          <?php if (!empty($title)): ?>
+            <h1 class="page-header"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
         <?php endif; ?>
-        <?php print render($title_suffix); ?>
         <?php print $messages; ?>
         <?php if (!empty($tabs)): ?>
           <?php print render($tabs); ?>
