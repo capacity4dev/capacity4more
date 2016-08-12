8b180648a95b851409337a18c21d1341d52a38da
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-08-12 15:24:40 +0200

    CFM-607: CSS and text fixes.

--- project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_group_text_cta.tpl.php
+++ project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_group_text_cta.tpl.php
@@ -5,7 +5,7 @@
  * Template to render the CTA block for a visitor.
  */
 ?>
-<div class="c4m-group-text-cta text-xsmall">
+<div class="c4m-group-text-cta text-copy--small">
   <i class="fa <?php print $icon; ?>"></i>
   <?php print $text; ?>
 </div>
