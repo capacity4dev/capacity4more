da1466ba3f2c402d92d278813ce3b36ae4b5cc99
Nir Galon <nirgn975@gmail.com>
2016-08-14 16:15:12 +0300

    Revert c4m_og_vocab.

--- project/profiles/capacity4more/modules/c4m/content/c4m_content_group/templates/c4m_og_vocab.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_group/templates/c4m_og_vocab.tpl.php
@@ -5,13 +5,11 @@
  * Template for the Vocabulary topics.
  */
 ?>
-<div class="">
-  <p class="taxonomy-title"><?php print $tree['root'] ?></p>
+<div>
+  <p class="taxonomy-title"><?php print l($root->name, "/taxonomy/term/" . $root->tid); ?></p>
 
-  <?php foreach ($tree['children'] as $child): ?>
-    <div class="taxonomy-item">
-      <i class="fa fa-caret-right" aria-hidden="true"></i>
-      <?php print $child; ?>
-    </div>
-  <?php endforeach; ?>
+  <div class="taxonomy-item">
+    <i class="fa fa-caret-right" aria-hidden="true"></i>
+    <?php print $tree; ?>
+  </div>
 </div>
