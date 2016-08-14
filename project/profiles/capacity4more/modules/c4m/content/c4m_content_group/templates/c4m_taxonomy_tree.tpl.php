112e433b6bc7836dbdc1b61ea13a307dd9f54d7a
Nir Galon <nirgn975@gmail.com>
2016-08-14 15:25:17 +0300

    Style the tree.

--- project/profiles/capacity4more/modules/c4m/content/c4m_content_group/templates/c4m_taxonomy_tree.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_group/templates/c4m_taxonomy_tree.tpl.php
@@ -9,7 +9,9 @@
   <p class="taxonomy-title"><?php print $root ?></p>
 
   <?php if (!empty($local_tree)): ?>
-    <i class="fa fa-caret-right" aria-hidden="true"></i>
-    <?php print $local_tree; ?>
+    <p class="taxonomy-item">
+      <i class="fa fa-caret-right" aria-hidden="true"></i>
+      <?php print $local_tree; ?>
+    </p>
   <?php endif; ?>
 </div>
