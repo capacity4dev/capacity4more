e64e52850c0b52742b715a262a4b473bf4893124
Or David <or@gizra.com>
2016-09-01 14:09:06 +0300

    Fix the PR.

--- project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_group_text_cta.tpl.php
+++ project/profiles/capacity4more/modules/c4m/user/c4m_user_og/templates/c4m_group_text_cta.tpl.php
@@ -5,8 +5,8 @@
  * Template to render the CTA block for a visitor.
  */
 ?>
-<div class="<?php print $class; ?>">
-  <span class="<?php print $span_class; ?>">
+<div class="<?php print $wrapper_class; ?>">
+  <span class="<?php print $text_class; ?>">
       <i class="fa <?php print $icon; ?>"></i>
     <?php print $text; ?>
   </span>
