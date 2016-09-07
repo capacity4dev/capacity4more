89d8763c8027bf2aae8fe4b08117c4d6ff28eb11
Jochen Verdeyen <jochen.verdeyen@amplexor.com>
2016-09-07 11:06:38 +0200

    CFM-1181: Update using template file

---
+++ project/profiles/capacity4more/modules/c4m/features/c4m_features_social/theme/c4m-features-social-actions-share.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Template file for "c4m_features_social_actions_share".
+ */
+
+/**
+ * Available variables:
+ *  - $json_data: JSON data
+ */
+
+?>
+<div class="social-actions-share">
+  <h3><?php print t('Share this page'); ?></h3>
+  <script type="application/json"><?php print $json_data; ?></script>
+  <div id="c4m-print">
+    <div class="share_list share_style_link share_style_icon_24">
+      <a href="#" title="Print" onclick="window.print();return false;"></a>
+    </div>
+  </div>
+</div>
