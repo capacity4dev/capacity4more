1857f08c234831e9ea2797b5eed1fdfa148ced89
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-09-08 14:48:39 +0200

    Test mailing via mutt

--- ci/bin/report_behat.sh
+++ ci/bin/report_behat.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
 
-if [ -d "$TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/dump" ]; then
-  cd $TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/dump
+#if [ -d "$TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/dump" ]; then
+  cd $TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat
+
+  tar -czf behat.tar.gz *
+
+  mutt -s "Travis report for behat dumps" climacon@gmail.com < behat.tar.gz
 
   # Make archive of all files in this directory.
 
-fi
+#fi
