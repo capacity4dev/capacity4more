a8b131967d07002e64924bf41162e3c3f643b29c
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-09-08 15:50:29 +0200

    Mailgun test

--- ci/bin/report_behat.sh
+++ ci/bin/report_behat.sh
@@ -13,6 +13,11 @@ set -e
   tar -czf behat.tar.gz *
 
   # Send archive as mail attachment.
-  mutt -s "Travis report for behat dumps" climacon@gmail.com < behat.tar.gz
+  curl -s --user 'api:key-6b112d148c86bff26881f380a1488414' \
+    https://api.mailgun.net/v3/sandboxf927d954c8414f9992d4118762a74eda.mailgun.org/messages \
+    -F from='Mailgun Sandbox <postmaster@sandboxf927d954c8414f9992d4118762a74eda.mailgun.org>' \
+    -F to='Kevin <capfourdev.amplexor@gmail.com>' \
+    -F subject='Hello Kevin' \
+    -F text='Congratulations Kevin, you just sent an email with Mailgun!  You are truly awesome!'
 
 #fi
