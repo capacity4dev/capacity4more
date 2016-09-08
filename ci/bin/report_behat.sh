22b1f1c2917fcc94d05b28bd945bc31fe95b412b
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-09-08 15:10:08 +0200

    TEst empty file

--- ci/bin/report_behat.sh
+++ ci/bin/report_behat.sh
@@ -2,9 +2,12 @@
 set -e
 
 #if [ -d "$TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/dump" ]; then
+  date
+
   cd $TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat
 
   # Make archive of all files in this directory.
+  touch test.txt
   tar -czf behat.tar.gz *
 
   # Send archive as mail attachment.
