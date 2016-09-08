716d6785c544ee387e74c994023916dfa91d5f36
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-09-08 14:54:32 +0200

    Only test mail.

--- ci/bin/report_behat.sh
+++ ci/bin/report_behat.sh
@@ -4,10 +4,10 @@ set -e
 #if [ -d "$TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/dump" ]; then
   cd $TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat
 
+  # Make archive of all files in this directory.
   tar -czf behat.tar.gz *
 
+  # Send archive as mail attachment.
   mutt -s "Travis report for behat dumps" climacon@gmail.com < behat.tar.gz
-
-  # Make archive of all files in this directory.
 
 #fi
