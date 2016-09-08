e0192447ec58ff813bad62527399ecad13af1105
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-09-08 16:39:03 +0200

    Test dump files via mailgun

--- ci/bin/report_behat.sh
+++ ci/bin/report_behat.sh
@@ -12,6 +12,8 @@ cat test.txt
 
   tar -czf behat.tar.gz test.txt
 
+  ls -al
+
   # Send archive as mail attachment.
   curl -s --user 'api:key-6b112d148c86bff26881f380a1488414' \
     https://api.mailgun.net/v3/sandboxf927d954c8414f9992d4118762a74eda.mailgun.org/messages \
@@ -19,5 +21,6 @@ cat test.txt
     -F to='capacity4more admin <capfourdev.amplexor@gmail.com>' \
     -F subject='Behat reporting - Error dumps' \
     -F text='Attached you can find the behat error dumps!' \
-    -F attachment=@files/behat.tar.gz
+    -F attachment=@"behat.tar.gz" \
+    -F attachment=@test.txt
 #fi
