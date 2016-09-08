cedd03d8a5e80005691ad71806041adc39975054
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-09-08 14:36:19 +0200

    Try to install dumps and mailing client in Travis.

---
+++ ci/bin/report_behat.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+if [ -d "$TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/dump" ]; then
+  cd $TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/dump
+
+  # Make archive of all files in this directory.
+
+fi
