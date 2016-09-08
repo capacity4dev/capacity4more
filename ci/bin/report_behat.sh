23b276817aca2e22aa47db03037d517d5bf63907
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-09-08 16:19:34 +0200

    Test dump files via mailgun

--- ci/bin/report_behat.sh
+++ ci/bin/report_behat.sh
@@ -1,27 +1,19 @@
 #!/bin/sh
 set -e
 
-#if [ -d "$TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/dumps" ]; then
+if [ -d "$TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/features/dump" ]; then
   date
 
-#  cd $TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/dumps
+  cd $TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/features/dump
 
-# Debug feedback.
-  pwd
-  ls -al
-
-#  tar -czf behat.tar.gz *
-
-echo "Hello world" > ./testfile.txt
-cat ./testfile.txt
+  tar -czf behat.tar.gz *
 
   # Send archive as mail attachment.
   curl -s --user 'api:key-6b112d148c86bff26881f380a1488414' \
     https://api.mailgun.net/v3/sandboxf927d954c8414f9992d4118762a74eda.mailgun.org/messages \
     -F from='Mailgun Sandbox <postmaster@sandboxf927d954c8414f9992d4118762a74eda.mailgun.org>' \
-    -F to='Kevin <capfourdev.amplexor@gmail.com>' \
+    -F to='capacity4more admin <capfourdev.amplexor@gmail.com>' \
     -F subject='Behat reporting - Error dumps' \
     -F text='Attached you can find the behat error dumps!' \
-    -F attachment=testfile.txt
-
-#fi
+    -F attachment=behat.tar.gz
+fi
