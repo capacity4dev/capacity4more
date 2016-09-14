88b50d57b34a302b26b8edd22db2f3ae0937e398
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-09-14 09:21:04 +0200

    Add variables to the reportings

--- ci/bin/report_behat.sh
+++ ci/bin/report_behat.sh
@@ -14,14 +14,14 @@ if [ -d "$TRAVIS_BUILD_DIR/project/profiles/capacity4more/behat/features/dump" ]
   NOW=$(date +"%Y%m%d-%H%M%s")
   FILE="behat.$NOW.tar.gz"
 
-  tar -czf behat.$NOW.tar.gz *
+  tar -czf behat.$TRAVIS_BUILD_ID.$TRAVIS_BUILD_NUMBER--$NOW.tar.gz *
 
   # Send archive as mail attachment.
-  curl -s --user 'api:key-6b112d148c86bff26881f380a1488414' \
-    https://api.mailgun.net/v3/sandboxf927d954c8414f9992d4118762a74eda.mailgun.org/messages \
-    -F from='Mailgun Sandbox <postmaster@sandboxf927d954c8414f9992d4118762a74eda.mailgun.org>' \
-    -F to='capacity4more admin <capfourdev.amplexor@gmail.com>' \
-    -F subject='Behat reporting - Error dumps' \
-    -F text='Attached you can find the behat error dumps!' \
-    -F attachment=@behat.$NOW.tar.gz
+  curl -s --user "api:$MAILGUN_API" \
+    $MAILGUN_MESSAGES_URL \
+    -F from=$MAILGUN_FROM \
+    -F to=$MAILGUN_TO \
+    -F subject="Behat reporting - Error dumps for build $TRAVIS_BUILD_ID / $TRAVIS_BUILD_NUMBER" \
+    -F text="Attached you can find the behat error dumps for build $TRAVIS_BUILD_ID / $TRAVIS_BUILD_NUMBER!" \
+    -F attachment=@$FILE
 fi
