341085f8d2195073c3e4fc84719e378119930da7
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-09-28 10:01:46 +0200

    Try to install g++ 4.9 at least

--- ci/bin/install_nodejs.sh
+++ ci/bin/install_nodejs.sh
@@ -8,4 +8,5 @@ set -e
 # ---------------------------------------------------------------------------- #
 
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y g++-4.9
 sudo apt-get install -y nodejs
