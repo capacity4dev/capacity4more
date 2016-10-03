249438c640ff9f84e1e2413da2c916fca22a2d14
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-10-03 13:59:00 +0200

    Remove plupload examples folder.

--- config/build/drupal_make_after.sh
+++ config/build/drupal_make_after.sh
@@ -38,5 +38,9 @@ markup_h2 "Robots.txt"
 cp -a "$DIR_ROOT/config/build/settings/robots.txt" "$DIR_WEB"
 message_success "Custom robots.txt file copied to web folder."
 
+markup_h2 "Plupload examples folder"
+rm -Rf "$DIR_WEB/sites/all/libraries/plupload/examples"
+message_success "Removed plupload examples folder from web directory."
+
 
 echo
