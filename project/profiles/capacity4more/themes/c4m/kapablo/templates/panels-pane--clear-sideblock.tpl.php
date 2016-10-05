fcd60c0610af1f5f839d32aab99b35b17f7a607c
Jochen Verdeyen <jochen.verdeyen@amplexor.com>
2016-10-05 09:54:51 +0200

    CFM-1543: Code Style update

--- project/profiles/capacity4more/themes/c4m/kapablo/templates/panels-pane--clear-sideblock.tpl.php
+++ project/profiles/capacity4more/themes/c4m/kapablo/templates/panels-pane--clear-sideblock.tpl.php
@@ -28,10 +28,10 @@
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
   <<?php print $title_heading; ?><?php print $title_attributes; ?>>
-  <?php print $title; ?>
-</<?php print $title_heading; ?>>
-<?php endif; ?>
-<?php print render($title_suffix); ?>
+    <?php print $title; ?>
+  </<?php print $title_heading; ?>>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
 
 <?php if ($feeds): ?>
   <?php print $feeds; ?>
