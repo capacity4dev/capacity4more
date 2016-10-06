3eaaef372d4570bf0c01a7c8e71a9c055257b45a
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-10-06 14:36:06 +0200

    Add issets to the project management pages.

--- project/profiles/capacity4more/modules/c4m/features_og/c4m_features_og_manage/plugins/content_types/project_details/project-details.tpl.php
+++ project/profiles/capacity4more/modules/c4m/features_og/c4m_features_og_manage/plugins/content_types/project_details/project-details.tpl.php
@@ -13,78 +13,87 @@
         <h2><?php print t('Project administration') ?></h2>
       </div>
       <div class="row">
-        <div class="col-md-3"><?php print t('Owner') ?> (<a href="<?php print $details['owner_edit']; ?>"><?php print t('Edit') ?></a>)</div>
+        <div class="col-md-3"><?php print t('Owner') ?> (<a
+            href="<?php print $details['owner_edit']; ?>"><?php print t(
+              'Edit'
+            ) ?></a>)
+        </div>
         <div class="col-md-9"><?php print $details['owner']; ?></div>
       </div>
       <div class="row">
-        <div class="col-md-3"><?php print t('Members') ?> (<a href="<?php print $details['members_edit']; ?>"><?php print t('Edit') ?></a>)</div>
+        <div class="col-md-3"><?php print t('Members') ?> (<a
+            href="<?php print $details['members_edit']; ?>"><?php print t(
+              'Edit'
+            ) ?></a>)
+        </div>
         <div class="col-md-9"><?php print $details['members']; ?></div>
       </div>
     </div>
     <div class="project-details">
       <div class="title">
         <h2><?php print t('Project details') ?></h2>
-        <a href="<?php print $details['edit_link'] ?>#edit-details"><?php print t('Edit') ?></a>
+        <a href="<?php print $details['edit_link'] ?>#edit-details"><?php print t('Edit'); ?></a>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('Project name') ?></div>
-        <div class="col-md-9"><?php print $details['title'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['title'])) ? $details['title'] : ''; ?></div>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('Project path') ?></div>
-        <div class="col-md-9"><?php print $details['purl'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['purl'])) ? $details['purl'] : ''; ?></div>
       </div>
-      <?php if (isset($details['description']['safe_value'])): ?>
-        <div class="row">
-          <div class="col-md-3"><?php print t('Project description') ?></div>
-          <div class="col-md-9"><?php print $details['description']['safe_value'] ?></div>
-        </div>
-      <?php endif; ?>
+      <div class="row">
+        <div class="col-md-3"><?php print t('Project description') ?></div>
+        <div class="col-md-9"><?php print isset($details['description']['safe_value']) ? $details['description']['safe_value'] : ''; ?></div>
+      </div>
       <div class="row">
         <div class="col-md-3"><?php print t('Duration') ?></div>
-        <div class="col-md-9"><?php print $details['duration'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['duration'])) ? $details['duration'] : ''; ?></div>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('Total budget') ?></div>
-        <div class="col-md-9"><?php print $details['total_budget'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['total_budget'])) ? $details['total_budget'] : ''; ?></div>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('Source of funding') ?></div>
-        <div class="col-md-9"><?php print $details['source_funding'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['source_funding'])) ? $details['source_funding'] : ''; ?></div>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('Partners') ?></div>
-        <div class="col-md-9"><?php print $details['partners'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['partners'])) ? $details['partners'] : ''; ?></div>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('CRIS Decision number') ?></div>
-        <div class="col-md-9"><?php print $details['cris_decision_number'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['cris_decision_number'])) ? $details['cris_decision_number'] : ''; ?></div>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('CRIS Contact number') ?></div>
-        <div class="col-md-9"><?php print $details['cris_contact_number'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['cris_contact_number'])) ? $details['cris_contact_number'] : ''; ?></div>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('Project topics') ?></div>
-        <div class="col-md-9"><?php print $details['topics'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['topics'])) ? $details['topics'] : ''; ?></div>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('Project regions & countries') ?></div>
-        <div class="col-md-9"><?php print $details['locations'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['locations'])) ? $details['locations'] : ''; ?></div>
       </div>
     </div>
     <div class="project-related-content">
       <div class="title">
         <h2><?php print t('Related content') ?></h2>
-        <a href="<?php print $details['edit_link'] ?>#edit-related-content"><?php print t('Edit') ?> </a>
+        <a
+          href="<?php print $details['edit_link'] ?>#edit-related-content"><?php print t(
+            'Edit'
+          ) ?> </a>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('Related Groups') ?></div>
-        <div class="col-md-9"><?php print $details['related_groups'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['related_groups'])) ? $details['related_groups'] : ''; ?></div>
       </div>
       <div class="row">
         <div class="col-md-3"><?php print t('Related Initiatives') ?></div>
-        <div class="col-md-9"><?php print $details['related_projects'] ?></div>
+        <div class="col-md-9"><?php print (isset($details['related_projects'])) ? $details['related_projects'] : ''; ?></div>
       </div>
     </div>
     <div class="project-related-content">
@@ -93,7 +102,13 @@
         <?php print $details['taxonomy_manage_link'] ?>
       </div>
       <div class="row">
-        <div class="col-md-12"><?php print t('This project has <strong>@cat_count Categories</strong> and <strong>@term_count Terms</strong>', array('@cat_count' => $details['categories_count'], '@term_count' => $details['terms_count'])) ?></div>
+        <div class="col-md-12"><?php print t(
+            'This project has <strong>@cat_count Categories</strong> and <strong>@term_count Terms</strong>',
+            array(
+              '@cat_count' => $details['categories_count'],
+              '@term_count' => $details['terms_count'],
+            )
+          ) ?></div>
       </div>
     </div>
     <div class="project-features">
@@ -104,14 +119,19 @@
       <?php foreach ($details['features_available'] as $feature) : ?>
         <div class="row">
           <div class="col-md-3"><?php print $feature['name']; ?></div>
-          <div class="col-md-9"><?php print empty($details['features_enabled'][$feature['machine_name']]) ? t('Disabled') : '<strong>' . t('Enabled') . '</strong>'; ?></div>
+          <div
+            class="col-md-9"><?php print empty($details['features_enabled'][$feature['machine_name']]) ? t(
+              'Disabled'
+            ) : '<strong>' . t('Enabled') . '</strong>'; ?></div>
         </div>
       <?php endforeach; ?>
     </div>
     <div class="project-navigation">
       <div class="title">
         <h2><?php print t('Project Navigation') ?></h2>
-        <a href="<?php print $details['menu_link']; ?>"><?php print t('Edit') ?></a>
+        <a href="<?php print $details['menu_link']; ?>"><?php print t(
+            'Edit'
+          ) ?></a>
       </div>
     </div>
   </div>
