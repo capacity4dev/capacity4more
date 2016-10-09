3b24c8fdf7545dd3e30c14f865250ab569ef1cea
anatoly vaitsman <anatoly@gizra.com>
2016-10-09 19:21:25 +0300

    Coder

--- project/profiles/capacity4more/modules/c4m/og/c4m_og_group_menu/js/c4m-wiki-select.js
+++ project/profiles/capacity4more/modules/c4m/og/c4m_og_group_menu/js/c4m-wiki-select.js
@@ -8,7 +8,7 @@
   // it's value as a path link.
   Drupal.behaviors.wikiSelect = {
     attach: function (context) {
-      $("#edit-wiki-select").on("change", function() {
+      $("#edit-wiki-select").on("change", function () {
         var $val = $("#edit-wiki-select").val();
         $("#edit-link-path").val($val);
       })
