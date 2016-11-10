f676846effae77641b9c40d515300f260cc7fd21
Or David <or@gizra.com>
2016-11-10 12:34:24 +0200

    Fixing the upcoming events block on homepage.

--- project/profiles/capacity4more/modules/c4m/features_og/c4m_features_og_events/plugins/content_types/upcoming_events/upcoming_events.tpl.php
+++ project/profiles/capacity4more/modules/c4m/features_og/c4m_features_og_events/plugins/content_types/upcoming_events/upcoming_events.tpl.php
@@ -9,7 +9,11 @@
 <div class="sidebarblock upcoming-events">
   <h2 class="sidebarblock__title closely"><?php print t('Upcoming events') ?></h2>
   <?php print $events; ?>
+
+  <?php if ($display_see_more): ?>
   <div class="sidebarblock__viewmore">
     <a class="see-more-link" href="<?php print url('events/upcoming', array('absolute' => TRUE, 'query' => drupal_get_query_parameters())); ?>"><?php print t('See more') ?> <i class="fa fa-chevron-right"></i></a>
   </div>
+  <?php endif; ?>
+
 </div>
