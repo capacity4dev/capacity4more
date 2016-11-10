ec26bf3b09e02de30a5e9b070ea1fbd645027913
yBaras <yoav@gizra.com>
2016-11-10 18:56:05 +0200

    [ci skip] Move control structure.

--- project/profiles/capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/my_groups/my-groups.tpl.php
+++ project/profiles/capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/my_groups/my-groups.tpl.php
@@ -10,7 +10,7 @@
   <h2 class="sidebarblock__title closely"><?php print t('My Groups') ?></h2>
   <?php print $groups; ?>
   <div class="sidebarblock__viewmore">
-    <?php if ($num_groups > 5): ?>
+    <?php if ($display_see_more): ?>
     <a class="see-more-link" href="<?php print $link; ?>"><?php print t('Show more') ?> <i class="fa fa-chevron-right"></i></a>
     <?php endif; ?>
   </div>
