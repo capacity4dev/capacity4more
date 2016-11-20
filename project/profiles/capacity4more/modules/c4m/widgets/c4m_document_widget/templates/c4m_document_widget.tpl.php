439f43f7752ea9178352c6e680da81568f344374
anatoly vaitsman <anatoly@gizra.com>
2016-11-20 18:31:07 +0200

    Adjust document widget for style accessibility

--- project/profiles/capacity4more/modules/c4m/widgets/c4m_document_widget/templates/c4m_document_widget.tpl.php
+++ project/profiles/capacity4more/modules/c4m/widgets/c4m_document_widget/templates/c4m_document_widget.tpl.php
@@ -5,8 +5,8 @@
  * Template to render the document widget.
  */
 ?>
-<div class="form-group input-wrapper file-wrapper" id="add-document-form">
-  <label class="control-label"><?php print $title; ?></label>
+<div class="form-group input-wrapper file-wrapper"">
+  <label for="<?php print $field_name; ?>" class="control-label"><?php print $title; ?></label>
   <?php if ($required): ?>
     <span class="form-required" title="<?php print t('This field is required.'); ?>">*</span>
   <?php endif; ?>
@@ -14,7 +14,7 @@
       <div class="cfm-file-upload" ng-file-drop="onFileSelect($files, '<?php print $field_name; ?>');"
            ng-file-drag-over-class="file-upload-drag">
         <input type="file" name="document-file" id="<?php print $field_name; ?>" class="document_file" ng-file-select="onFileSelect($files, '<?php print $field_name; ?>')">
-        <i class="fa fa-hand-pointer-o cfm-file-upload__hand" aria-hidden="true"></i>
+        <i class="fa fa-hand-pointer-o cfm-file-upload__hand"></i>
         <p class="cfm-file-upload__title"><?php print t('Drop file here to upload documents'); ?></p>
         <a href="javascript://" ng-click="browseFiles('<?php print $field_name; ?>')"> <?php print t('or browse'); ?> </a>
       </div>
@@ -41,5 +41,5 @@
     </div>
   <related-documents related-documents="data.relatedDocuments['<?php print $field_name; ?>']" form-id="formId" field-name="'<?php print $field_name; ?>'"></related-documents>
 
-  <input type="text" id="input-<?php print $field_name; ?>" class="hidden-input" value="<?php print $default_value; ?>">
+  <input type="hidden" id="input-<?php print $field_name; ?>" class="hidden-input" value="<?php print $default_value; ?>">
 </div>
