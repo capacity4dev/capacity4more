17dee452a69f720c9a5216553c44a494386a6694
Or David <ordavidil@gmail.com>
2016-11-21 18:57:41 +0200

    Fixing path of all templates url.

--- project/profiles/capacity4more/modules/c4m/restful/c4m_restful_quick_post/components/c4m-app/src/directives/calendar/calendar.js
+++ project/profiles/capacity4more/modules/c4m/restful/c4m_restful_quick_post/components/c4m-app/src/directives/calendar/calendar.js
@@ -17,7 +17,7 @@
 angular.module('c4mApp')
   .directive('calendar', function ($window, DrupalSettings) {
     return {
-      templateUrl: DrupalSettings.getBasePath() + 'profiles/capacity4more/libraries/bower_components/c4m-app/dist/directives/calendar/calendar.html',
+      templateUrl: DrupalSettings.getBasePath() + 'profiles/capacity4more/modules/c4m/restful/c4m_restful_quick_post/components/c4m-app/dist/directives/calendar/calendar.html',
       restrict: 'E',
       scope: true
     };
