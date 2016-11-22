dbd459478bd1d560780c7eb5d3036b0af6ddbc0e
anatoly vaitsman <anatoly@gizra.com>
2016-11-22 14:34:47 +0200

    Add title attribute to document widget elements [ci skip]

--- project/profiles/capacity4more/modules/c4m/widgets/c4m_document_widget/templates/c4m_document_widget.tpl.php
+++ project/profiles/capacity4more/modules/c4m/widgets/c4m_document_widget/templates/c4m_document_widget.tpl.php
@@ -6,17 +6,17 @@
  */
 ?>
 <div class="form-group input-wrapper file-wrapper"">
-  <label for="<?php print $field_name; ?>" class="control-label"><?php print $title; ?></label>
+  <label for="<?php print $field_name; ?>" class="control-label" title="<?php print $title; ?>"><?php print $title; ?></label>
   <?php if ($required): ?>
     <span class="form-required" title="<?php print t('This field is required.'); ?>">*</span>
   <?php endif; ?>
     <div class="cfm-file-upload-wrapper">
       <div class="cfm-file-upload" ng-file-drop="onFileSelect($files, '<?php print $field_name; ?>');"
            ng-file-drag-over-class="file-upload-drag">
-        <input type="file" name="document-file" id="<?php print $field_name; ?>" class="document_file" ng-file-select="onFileSelect($files, '<?php print $field_name; ?>')">
+        <input type="file" name="document-file" id="<?php print $field_name; ?>" class="document_file" ng-file-select="onFileSelect($files, '<?php print $field_name; ?>')" title="Upload file">
         <i class="fa fa-hand-pointer-o cfm-file-upload__hand"></i>
-        <p class="cfm-file-upload__title"><?php print t('Drop file here to upload documents'); ?></p>
-        <a href="javascript://" ng-click="browseFiles('<?php print $field_name; ?>')"> <?php print t('or browse'); ?> </a>
+        <p class="cfm-file-upload__title" title="<?php print t('Drop file here to upload documents'); ?>"><?php print t('Drop file here to upload documents'); ?></p>
+        <a href="javascript://" ng-click="browseFiles('<?php print $field_name; ?>')" title="Browse"> <?php print t('or browse'); ?> </a>
       </div>
       <script>
         /**
@@ -35,7 +35,8 @@
       </script>
       <a href="JavaScript://" onclick="Drupal.media.popups.mediaBrowser(c4m_attachment_overlay,media_browser_options)"
          ng-click="setFieldName('<?php print $field_name; ?>')" id="link-<?php print $field_name; ?>"
-         ng-class="{'active-library-link':fieldName === '<?php print $field_name; ?>'}">
+         ng-class="{'active-library-link':fieldName === '<?php print $field_name; ?>'}"
+      title="<?php print t('Select a document from the library') ?>">
         <?php print t('or select a document from the library') ?>
       </a>
     </div>
