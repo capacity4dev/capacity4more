1f70e9b1924f536e3e59b7b50b84dcb23a99522c
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2016-11-24 15:13:35 +0100

    THEME: My Project pane.

--- project/profiles/capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/my_projects/my-projects.tpl.php
+++ project/profiles/capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/my_projects/my-projects.tpl.php
@@ -7,11 +7,16 @@
 ?>
 
 <div class="sidebarblock my-projects">
-  <h2 class="sidebarblock__title closely"><?php print t('My Projects & Programmes') ?></h2>
+  <h2 class="sidebarblock__title closely">
+    <?php print t('My Projects & Programmes') ?>
+  </h2>
   <?php print $projects; ?>
-  <div class="sidebarblock__viewmore">
-    <?php if ($display_see_more): ?>
-    <a class="see-more-link" href="<?php print $link; ?>"><?php print t('Show more') ?> <i class="fa fa-chevron-right"></i></a>
-    <?php endif; ?>
-  </div>
+
+  <?php if ($display_see_more): ?>
+    <div class="sidebarblock__viewmore">
+      <a class="see-more-link" href="<?php print $link; ?>">
+        <?php print t('Show more') ?> <i class="fa fa-chevron-right"></i>
+      </a>
+    </div>
+  <?php endif; ?>
 </div>
