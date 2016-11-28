d44767c3e6a635023fb16682b22f4956fb12d864
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2016-11-28 10:34:08 +0100

    THEME: Group manage page.

--- project/profiles/capacity4more/modules/c4m/features_og/c4m_features_og_manage/plugins/content_types/group_status/group-status.tpl.php
+++ project/profiles/capacity4more/modules/c4m/features_og/c4m_features_og_manage/plugins/content_types/group_status/group-status.tpl.php
@@ -8,16 +8,16 @@
 
 <div class="row group-management-dashboard">
   <div class="col-md-12 group-management-wrapper right">
-    <div class="group-status panel-pane">
+    <div class="group-status-wrapper panel-pane">
       <div class="title">
         <h2><?php print t('Group status') ?></h2>
       </div>
       <div class="row">
-        <div class="col-md-4 group-status-text"><?php print $info['status'] ?></div>
-        <div class="col-md-8 archive-group"><?php print $info['archive_link'] ?></div>
+        <div class="col-md-4 group-status-text"><span class="label label-default"><?php print $info['status'] ?></span></div>
+        <div class="col-md-8 archive-group text-right"><?php print $info['archive_link'] ?></div>
       </div>
       <div class="row">
-        <div class="col-md-12"><?php print $info['pending_members_link'] ?> <?php print t('are waiting for approval.') ?></div>
+        <div class="col-md-12 text-justify"><?php print $info['pending_members_link'] ?> <?php print t('waiting for approval.') ?></div>
       </div>
     </div>
     <div class="thumbnail-image panel-pane">
