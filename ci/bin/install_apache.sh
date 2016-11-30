50da3c3763de2fcce8f5154d4ff0a75ee8199918
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2016-11-30 20:51:30 +0100

    Try very specific version.

--- ci/bin/install_apache.sh
+++ ci/bin/install_apache.sh
@@ -13,36 +13,49 @@ if [ $INSTALL_PROFILE != 1 ]; then
  exit 0;
 fi
 
-# Install necesary php packages.
-sudo apt-get install -y --force-yes php5-cgi php5-mysql
-
-# Install Apache.
-sudo apt-get install apache2 libapache2-mod-fastcgi
-sudo a2enmod rewrite actions fastcgi alias
-
 # Config php-fpm.
 
 php_version=$(phpenv version-name)
 
-if [ "$php_version" = 7.0 ]; then
+if [ "$php_version" = 7.0.13 ]; then
+    # Install necesary php packages.
+    sudo apt-get install -y --force-yes php7.0-cgi php7.0-mysql
+
+    # Install Apache.
+    sudo apt-get install apache2 libapache2-mod-fastcgi
+    sudo a2enmod rewrite actions fastcgi alias
+
     sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/php-fpm.conf
-else
-    sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
-fi
 
-echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+    echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
-# Set sendmail so php doesn't throw an error while trying to send out email.
-echo "sendmail_path='true'" >> `php --ini | grep "Loaded Configuration" | awk '{print $4}'`
+    # Set sendmail so php doesn't throw an error while trying to send out email.
+    echo "sendmail_path='true'" >> `php --ini | grep "Loaded Configuration" | awk '{print $4}'`
 
-# Create the www folder as we need it for the vhost config.
-mkdir $TRAVIS_BUILD_DIR/web
+    # Create the www folder as we need it for the vhost config.
+    mkdir $TRAVIS_BUILD_DIR/web
 
-# Create the default vhost config file.
-if [ "$php_version" = 7.0 ]; then
     sudo cp -f $TRAVIS_BUILD_DIR/ci/config/apache-70.conf /etc/apache2/sites-available/default
 else
+    # Install necesary php packages.
+    sudo apt-get install -y --force-yes php5-cgi php5-mysql
+
+    # Install Apache.
+    sudo apt-get install apache2 libapache2-mod-fastcgi
+    sudo a2enmod rewrite actions fastcgi alias
+
+    sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+
+    echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+
+    # Set sendmail so php doesn't throw an error while trying to send out email.
+    echo "sendmail_path='true'" >> `php --ini | grep "Loaded Configuration" | awk '{print $4}'`
+
+    # Create the www folder as we need it for the vhost config.
+    mkdir $TRAVIS_BUILD_DIR/web
+
     sudo cp -f $TRAVIS_BUILD_DIR/ci/config/apache.conf /etc/apache2/sites-available/default
 fi
 
