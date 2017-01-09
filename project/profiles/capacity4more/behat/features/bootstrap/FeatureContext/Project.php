08ff96337f2eb0f184e7c442ae89ed72897842b8
anatoly vaitsman <anatoly@gizra.com>
2017-01-09 03:07:22 +0200

    Fixing Behat tests

--- project/profiles/capacity4more/behat/features/bootstrap/FeatureContext/Project.php
+++ project/profiles/capacity4more/behat/features/bootstrap/FeatureContext/Project.php
@@ -22,6 +22,7 @@ trait Project {
     $steps[] = new Step\When('I visit "/node/' . $group->nid . '/edit"');
     $steps[] = new Step\When('I select "' . $status . '" from "edit-c4m-og-status-und"');
     $steps[] = new Step\When('I press "Save"');
+    $steps[] = new Step\When('I wait');
     $steps[] = new Step\When('I should see "Project ' . $group_title . ' has been updated."');
 
     return $steps;
