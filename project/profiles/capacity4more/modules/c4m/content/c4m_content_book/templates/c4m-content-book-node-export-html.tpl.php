0395085534a33d3341745acc9baf44b9783e0e32
Andrei Godea <andrei.godea@amplexor.com>
2017-12-20 01:25:58 +0200

    CFDRMD-57: Adjusted print pages.

--- project/profiles/capacity4more/modules/c4m/content/c4m_content_book/templates/c4m-content-book-node-export-html.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_book/templates/c4m-content-book-node-export-html.tpl.php
@@ -5,7 +5,12 @@
  * Template for the book export.
  */
 ?>
-<div class="book-print--item"><?php print render($content); ?></div>
+<div class="book-print--item">
+  <?php if (!empty($hierarchy)): ?>
+      <div class="book-print--navigation"><?php print $hierarchy; ?></div>
+  <?php endif; ?>
+  <?php print render($content); ?>
+</div>
 <?php if (!empty($children)) :?>
     <div class="book-print--children"><?php print render($children); ?></div>
 <?php endif; ?>
--- project/profiles/capacity4more/modules/c4m/content/c4m_content_book/templates/c4m-content-book-print-button.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_book/templates/c4m-content-book-print-button.tpl.php
@@ -11,9 +11,9 @@
     <i class="fa fa-print" aria-hidden="true"></i><span class="action">Print options</span>
     <span class="caret"></span>
   </a>
-  <ul class="dropdown-menu" aria-labelledby="dLabel">
-    <li><a href="javascript: w=window.open('/book/<?php print $nid; ?>/print?no_children=true');">Print page</a></li>
-    <li><a href="javascript: w=window.open('/book/<?php print $nid; ?>/print');">Print page & sub-pages</a></li>
-    <li><a href="javascript: w=window.open('/book/<?php print $bid; ?>/print');">Print all pages & sub-pages</a></li>
+  <ul class="dropdown-menu book-print-buttons" style="right: 0; left: auto;" aria-labelledby="dLabel">
+    <li><a href="/book/<?php print $nid; ?>/print?no_children=true" target="_blank" >Print page</a></li>
+    <li><a href="/book/<?php print $nid; ?>/print" target="_blank">Print page & sub-pages</a></li>
+    <li><a href="/book/<?php print $bid; ?>/print" target="_blank">Print all pages & sub-pages</a></li>
   </ul>
 </div>
