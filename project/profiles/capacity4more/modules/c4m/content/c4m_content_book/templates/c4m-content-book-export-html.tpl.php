12ed944a17358cae5389769c1a6dcd8d9dcc69cf
Andrei Godea <andrei.godea@amplexor.com>
2017-12-20 09:34:05 +0200

    CFDRMD-57: Added status on book print page.

--- project/profiles/capacity4more/modules/c4m/content/c4m_content_book/templates/c4m-content-book-export-html.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_book/templates/c4m-content-book-export-html.tpl.php
@@ -8,7 +8,19 @@
 drupal_add_js(drupal_get_path('module', 'c4m_content_book') . '/js/book-print.js');
 ?>
 <div class="book-print">
-    <?php print $contents; ?>
+    <?php if (!empty($status)): ?>
+        <div class="group-indications">
+            <div class="group-indications--access">
+                <i class="top-buffer group-icon group-public node-icon as-group-public"></i>
+                <span class="top-buffer indication label label-access public group-access">
+                    <?php print $status; ?>
+                </span>
+            </div>
+        </div>
+    <?php endif; ?>
+    <?php if (!empty($contents)): ?>
+        <?php print $contents; ?>
+    <?php endif; ?>
     <?php if (!empty($comments)): ?>
         <h2><?php print t("Comments"); ?></h2>
         <div class="book-print--comments">
