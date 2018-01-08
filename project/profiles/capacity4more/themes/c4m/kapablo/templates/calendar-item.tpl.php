673bb47fe2ff53b3744cda46514e980f64e7ce96
Andrei Godea <andrei.godea@amplexor.com>
2018-01-08 12:16:35 +0200

    CFDRMD-53: Added events calendar.

---
+++ project/profiles/capacity4more/themes/c4m/kapablo/templates/calendar-item.tpl.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Copy of calendar-item.tpl.php.
+ */
+
+$index = 0;
+?>
+<div class="<?php print !empty($item->class) ? $item->class : 'item'; ?>">
+  <div class="view-item view-item-<?php print $view->name ?>">
+    <?php
+      $element_style = '';
+      if (!empty($item->stripe[0])) {
+        $element_style = 'style="background-color:' . $item->stripe[0] . '"';
+      }
+    ?>
+    <div class="calendar <?php print $item->granularity; ?>view" <?php print $element_style; ?>>
+      <div class="<?php print $item->date_id ?> contents">
+        <?php foreach ($rendered_fields as $field): ?>
+          <?php if ($index++ == 0 && (isset($item->continuation) && $item->continuation)) : ?>
+          <?php endif;?>
+          <?php print $field; ?>
+        <?php endforeach; ?>
+      </div>
+      <?php if (isset($item->continues) && $item->continues) : ?>
+      <?php else : ?>
+      <?php endif;?>
+    </div>
+  </div>   
+</div>
